b25303928d8fc285897651612f5d04ac99348556
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-12-11 17:12:04 +0100

    add other test case

--- tests/test_data_generators.py
+++ tests/test_data_generators.py
@@ -78,3 +78,45 @@ def test_DataGeneratorAllSpectrums():
     A, B = test_generator.__getitem__(0)
     assert A[0].shape == A[1].shape == (10, 101), "Expected different data shape"
     assert B.shape[0] == 10, "Expected different label shape."
+
+def test_DataGeneratorAllSpectrums_input_error():
+    """Test if expected error is raised for incorrect input formats"""
+    # Get test data
+    spectrums_binned, score_array, inchikey_score_mapping, inchikeys_all = create_test_data()
+
+    # Define other parameters
+    batch_size = 10
+    num_turns = 1
+    peak_scaling = 0.5
+    dimension = 101
+    same_prob_bins = [(0, 0.5), (0.5, 1)]
+
+    spectrum_ids = np.arange(0,150)
+
+    # Create generator --> wrong score array size
+    with pytest.raises(AssertionError) as msg:
+        _ = DataGeneratorAllSpectrums(spectrums_binned, score_array[:-2, :-2], spectrum_ids,
+                                    inchikey_score_mapping, inchikeys_all,
+                                    dim=dimension, batch_size=batch_size,
+                                    num_turns=num_turns, peak_scaling=peak_scaling,
+                                    shuffle=True, ignore_equal_pairs=True,
+                                    same_prob_bins=same_prob_bins,
+                                    augment_peak_removal_max=0.0,
+                                    augment_peak_removal_intensity=0.0,
+                                    augment_intensity=0.0)
+    assert 'Expected score_array of size 100x100.' in str(msg.value), \
+        "Expected different expection to be raised"
+
+    # Create generator --> wrong score array size
+    with pytest.raises(AssertionError) as msg:
+        _ = DataGeneratorAllSpectrums(spectrums_binned[:-2], score_array, spectrum_ids,
+                                    inchikey_score_mapping, inchikeys_all,
+                                    dim=dimension, batch_size=batch_size,
+                                    num_turns=num_turns, peak_scaling=peak_scaling,
+                                    shuffle=True, ignore_equal_pairs=True,
+                                    same_prob_bins=same_prob_bins,
+                                    augment_peak_removal_max=0.0,
+                                    augment_peak_removal_intensity=0.0,
+                                    augment_intensity=0.0)
+    assert "inchikeys_all and spectrums_binned must have the same dimension." in str(msg.value), \
+        "Expected different expection to be raised"
