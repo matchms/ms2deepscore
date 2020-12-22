d84cf69314ea8a433236fa750f734d39928ccb9c
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-12-22 13:44:49 +0100

    add BinnedSpectrum class

---
+++ ms2deepscore/BinnedSpectrum.py
@@ -0,0 +1,39 @@
+class BinnedSpectrum:
+    """Binned spectrum for use with MS2DeepScore."""
+    def __init__(self, binned_peaks: dict, metadata: dict):
+        self.peaks = binned_peaks
+        self._metadata = metadata
+
+    def __eq__(self, other):
+        return \
+            self.peaks == other.peaks and \
+            self.metadata == other.metadata
+
+    def get(self, key: str, default=None):
+        """Retrieve value from :attr:`metadata` dict. Shorthand for
+
+        .. code-block:: python
+
+            val = self.metadata.get("key", default)
+
+        """
+        return self._metadata.get(key, default)
+
+    def set(self, key: str, value):
+        """Set value in :attr:`metadata` dict. Shorthand for
+
+        .. code-block:: python
+
+            self.metadata[key] = val
+
+        """
+        self._metadata[key] = value
+        return self
+
+    @property
+    def metadata(self):
+        return self._metadata.copy()
+
+    @metadata.setter
+    def metadata(self, value):
+        self._metadata = value
