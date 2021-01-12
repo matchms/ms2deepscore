92ba2893ee5065e0d9cc7534ff82f4b7cd053591
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-01-12 18:04:37 +0100

    update model test

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,24 +8,37 @@ from tests.test_data_generators import create_test_data
 
 def get_test_generator():
     # Get test data
-    spectrums_binned, score_array, inchikey_score_mapping, inchikeys_all = create_test_data()
+    spectrums_binned, score_array, inchikey_score_mapping = create_test_data()
 
     dimension = 101
     same_prob_bins = [(0, 0.5), (0.5, 1)]
     inchikey_ids = list(np.arange(0, 80))
 
     # Create generator
-    return DataGeneratorAllInchikeys(spectrums_binned, score_array, inchikey_ids,
-                                     inchikey_score_mapping, inchikeys_all,
-                                     dim=dimension, same_prob_bins=same_prob_bins)
+    test_generator = DataGeneratorAllInchikeys(spectrums_binned, score_array, inchikey_ids,
+                                               inchikey_score_mapping,
+                                               dim=dimension,
+                                               same_prob_bins=same_prob_bins)
+    return test_generator
 
 
 def test_siamese_model():
     test_generator = get_test_generator()
-    model = SiameseModel(input_dim=101, base_dims=(200, 200, 200),
-                         embedding_dim=200, dropout_rate=0.2)
+    model = SiameseModel(input_dim=101, base_dims=(200, 200, 200), embedding_dim=200,
+                         dropout_rate=0.2)
     model.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.001))
     model.summary()
+    #x, y = zip(*test_generator)
     model.fit(test_generator,
               validation_data=test_generator,
               epochs=2)
+    assert len(model.model.layers) == 4, "Expected different number of layers"
+    assert len(model.model.layers[2].layers) == 11, "Expected different number of layers"
+    assert model.model.input_shape == [(None, 101), (None, 101)], "Expected different input shape"
+
+    # Test base model ("encoder")
+    X, y = test_generator.__getitem__(0)
+    embeddings = model.base.predict(X[0])
+    assert isinstance(embeddings, np.ndarray), "Expected numpy array"
+    assert embeddings.shape[0] == test_generator.settings["batch_size"] == 32, "Expected different batch size"
+    assert embeddings.shape[1] == model.base.output_shape[1] == 200, "Expected different embedding size"
