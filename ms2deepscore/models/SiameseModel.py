316ade8a27332bcee84a917e3fe61df1b849a282
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-02-02 14:04:41 +0100

    refactor methods

--- ms2deepscore/models/SiameseModel.py
+++ ms2deepscore/models/SiameseModel.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 from tensorflow.python.keras.saving import hdf5_format
 
 from ms2deepscore import SpectrumBinner
-    
+
 
 class SiameseModel:
     """
@@ -15,13 +15,13 @@ class SiameseModel:
     'head' model computes the cosine similarity between the embeddings.
 
     Mimics keras.Model API.
-    
+
     For example:
-        
+
     .. code-block:: python
-        
+
         # Import data and reference scores --> spectrums & tanimoto_scores_df
-        
+
         # Create binned spectrums
         spectrum_binner = SpectrumBinner(1000, mz_min=10.0, mz_max=1000.0, peak_scaling=0.5)
         binned_spectrums = spectrum_binner.fit_transform(spectrums)
@@ -30,7 +30,7 @@ class SiameseModel:
         dimension = len(spectrum_binner.known_bins)
         test_generator = DataGeneratorAllSpectrums(binned_spectrums, tanimoto_scores_df,
                                                    dim=dimension)
-    
+
         # Create (and train) a Siamese model
         model = SiameseModel(spectrum_binner, base_dims=(600, 500, 400), embedding_dim=400,
                              dropout_rate=0.2)
@@ -45,7 +45,8 @@ class SiameseModel:
                  spectrum_binner: SpectrumBinner,
                  base_dims: Tuple[int, int, int] = (600, 500, 500),
                  embedding_dim: int = 400,
-                 dropout_rate: float = 0.5):
+                 dropout_rate: float = 0.5,
+                 keras_model: keras.Model = None):
         """
         Construct SiameseModel
 
@@ -60,24 +61,26 @@ class SiameseModel:
             Dimension of the embedding (i.e. the output of the base model)
         dropout_rate
             Dropout rate to be used in the base model
+        keras_model
+            When provided, this keras model will be used to construct the SiameseModel instance.
+            Default is None.
         """
         assert spectrum_binner.known_bins is not None, \
             "spectrum_binner does not contain known bins (run .fit_transform() on training data first!)"
         self.spectrum_binner = spectrum_binner
         self.input_dim = len(spectrum_binner.known_bins)
-        self.base = self._get_base_model(input_dim=self.input_dim,
-                                         dims=base_dims,
-                                         embedding_dim=embedding_dim,
-                                         dropout_rate=dropout_rate)
-        input_a = keras.layers.Input(shape=self.input_dim, name="input_a")
-        input_b = keras.layers.Input(shape=self.input_dim, name="input_b")
-        embedding_a = self.base(input_a)
-        embedding_b = self.base(input_b)
-        cosine_similarity = keras.layers.Dot(axes=(1, 1),
-                                             normalize=True,
-                                             name="cosine_similarity")([embedding_a, embedding_b])
-        self.model = keras.Model(inputs=[input_a, input_b], outputs=[cosine_similarity],
-                                 name='head')
+
+        if keras_model is None:
+            # Create base model
+            self.base = self._get_base_model(input_dim=self.input_dim,
+                                             dims=base_dims,
+                                             embedding_dim=embedding_dim,
+                                             dropout_rate=dropout_rate)
+            # Create head model
+            self.model = self._get_head_model(input_dim=self.input_dim,
+                                              base_model=self.base)
+        else:
+            self._construct_from_keras_model(keras_model)
 
     def save(self, filename: Union[str, Path]):
         """
@@ -111,6 +114,24 @@ class SiameseModel:
         embedding = keras.layers.Dense(embedding_dim, activation='relu', name='embedding')(
             embedding)
         return keras.Model(model_input, embedding, name='base')
+
+    @staticmethod
+    def _get_head_model(input_dim: int,
+                        base_model: keras.Model):
+        input_a = keras.layers.Input(shape=input_dim, name="input_a")
+        input_b = keras.layers.Input(shape=input_dim, name="input_b")
+        embedding_a = base_model(input_a)
+        embedding_b = base_model(input_b)
+        cosine_similarity = keras.layers.Dot(axes=(1, 1),
+                                             normalize=True,
+                                             name="cosine_similarity")([embedding_a, embedding_b])
+        return keras.Model(inputs=[input_a, input_b], outputs=[cosine_similarity],
+                           name='head')
+
+    def _construct_from_keras_model(self, keras_model):
+        assert isinstance(keras_model, keras.Model), "Expected keras model as input."
+        self.base = keras_model.layers[2]
+        self.model = keras_model
 
     def compile(self, *args, **kwargs):
         self.model.compile(*args, **kwargs)
--- ms2deepscore/models/load_model.py
+++ ms2deepscore/models/load_model.py
@@ -25,30 +25,20 @@ def load_model(filename: Union[str, Path]) -> SiameseModel:
         Filename. Expecting saved SiameseModel.
 
     """
+    def binner_from_json(binner_json):
+        # Reconstitute spectrum_binner from json
+        binner_dict = json.loads(binner_json)
+        spectrum_binner = SpectrumBinner(binner_dict["number_of_bins"],
+                                         binner_dict["mz_max"], binner_dict["mz_min"],
+                                         binner_dict["peak_scaling"],
+                                         binner_dict["allowed_missing_percentage"])
+        spectrum_binner.peak_to_position = {int(key): value for key, value in binner_dict["peak_to_position"].items()}
+        spectrum_binner.known_bins = binner_dict["known_bins"]
+        return spectrum_binner
+
     with h5py.File(filename, mode='r') as f:
         binner_json = f.attrs['spectrum_binner']
         keras_model = hdf5_format.load_model_from_hdf5(f)
 
-    # Reconstitute spectrum_binner
-    binner_dict = json.loads(binner_json)
-    spectrum_binner = SpectrumBinner(binner_dict["number_of_bins"],
-                                     binner_dict["mz_max"], binner_dict["mz_min"],
-                                     binner_dict["peak_scaling"],
-                                     binner_dict["allowed_missing_percentage"])
-    spectrum_binner.peak_to_position = {int(key): value for key, value in binner_dict["peak_to_position"].items()}
-    spectrum_binner.known_bins = binner_dict["known_bins"]
-
-    # Extract parameters for SiameseModel
-    embedding_dim = keras_model.layers[2].output_shape[1]
-    base_dims = []
-    for layer in keras_model.layers[2].layers:
-        if "dense" in layer.name:
-            base_dims.append(layer.output_shape[1])
-        elif "dropout" in layer.name:
-            dropout_rate = layer.rate
-
-    model = SiameseModel(spectrum_binner, base_dims, embedding_dim, dropout_rate)
-    # TODO: Now this creates a keras model in the SiameseModel.__init__() and then replaces this. Seems unefficient.
-    model.base = keras_model.layers[2]
-    model.model = keras_model
-    return model
+    spectrum_binner = binner_from_json(binner_json)
+    return SiameseModel(spectrum_binner, keras_model=keras_model)
