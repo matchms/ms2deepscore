7ba1fbfcf44e3c10cb50381969791308e0e4432c
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-02-26 09:32:04 +0100

    extend utils, update notebooks

--- ms2deepscore/utils.py
+++ ms2deepscore/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def create_peak_dict(peak_list):
     """ Create dictionary of merged peaks (keep max-intensity peak per bin).
     """
@@ -8,3 +11,20 @@ def create_peak_dict(peak_list):
         else:
             peaks[ID] = weight
     return peaks
+
+
+def is_valid_inchikey14(inchikey14: str) -> bool:
+ """Return True if string has format of inchikey14.
+
+ Parameters
+ ----------
+ inchikey14
+     Input string to test if it format of an inchikey14.
+ """
+ if inchikey14 is None:
+     return False
+
+ regexp = r"[A-Z]{14}"
+ if re.fullmatch(regexp, inchikey14):
+     return True
+ return False
