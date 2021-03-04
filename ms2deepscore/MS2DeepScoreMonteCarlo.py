3108627550df53a9256c2f57aadcd14214e40fa1
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-03-04 20:45:21 +0100

    add Monte-Carlo ensembling score

---
+++ ms2deepscore/MS2DeepScoreMonteCarlo.py
@@ -0,0 +1,153 @@
+from typing import List
+import numba
+import numpy as np
+from matchms import Spectrum
+from matchms.similarity.BaseSimilarity import BaseSimilarity
+from tensorflow.keras.models import Model
+from tqdm import tqdm
+
+from .vector_operations import cosine_similarity_matrix, mean_pooling, std_pooling
+from .typing import BinnedSpectrumType
+
+
+class MS2DeepScoreMonteCarlo(BaseSimilarity):
+    """Calculate MS2DeepScore ensemble similarity scores and STD between a reference
+    and a query using Monte-Carlo Dropout
+
+    Using a trained model, binned spectrums will be converted into spectrum
+    vectors using a deep neural network. The MS2DeepScore similarity is then
+    the cosine similarity score between two spectrum vectors.
+
+    Example code to calcualte MS2DeepScore similarities between query and reference
+    spectrums:
+
+    .. code-block:: python
+
+        from matchms import calculate_scores()
+        from matchms.importing import load_from_json
+        from ms2deepscore import MS2DeepScore
+        from ms2deepscore.models import load_model
+
+        # Import data
+        references = load_from_json("abc.json")
+        queries = load_from_json("xyz.json")
+
+        # Load pretrained model
+        model = load_model("model_file_123.hdf5")
+
+        similarity_measure = MS2DeepScoreMonteCarlo(model)
+        # Calculate scores and get matchms.Scores object
+        scores = calculate_scores(references, queries, similarity_measure)
+
+
+    """
+    def __init__(self, model, n_ensembles: int = 10, progress_bar: bool = True):
+        """
+
+        Parameters
+        ----------
+        model:
+            Expected input is a SiameseModel that has been trained on
+            the desired set of spectra. The model contains the keras deep neural
+            network (model.model) as well as the used spectrum binner (model.spectrum_binner).
+        n_ensembles
+            Number of embeddings to create for every spectrum using Monte-Carlo Dropout.
+            n_ensembles will lead to n_ensembles^2 scores of which the mean and STD will
+            be taken.
+        progress_bar:
+            Set to True to monitor the embedding creating with a progress bar.
+            Default is False.
+        """
+        self.model = model
+        self.partial_model = Model(model.base.inputs, model.base.layers[-1].output)
+        self.n_ensembles = n_ensembles
+        self.input_vector_dim = self.model.base.input_shape[1]  # TODO: later maybe also check against SpectrumBinner
+        self.output_vector_dim = self.model.base.output_shape[1]
+        self.progress_bar = progress_bar
+
+    def _create_input_vector(self, binned_spectrum: BinnedSpectrumType):
+        """Creates input vector for model.base based on binned peaks and intensities"""
+        X = np.zeros((1, self.input_vector_dim))
+
+        idx = np.array([int(x) for x in binned_spectrum.binned_peaks.keys()])
+        values = np.array(list(binned_spectrum.binned_peaks.values()))
+        X[0, idx] = values
+        return X
+
+    def pair(self, reference: Spectrum, query: Spectrum) -> float:
+        """Calculate the MS2DeepScore similaritiy between a reference and a query spectrum.
+
+        Parameters
+        ----------
+        reference:
+            Reference spectrum.
+        query:
+            Query spectrum.
+
+        Returns
+        -------
+        ms2ds_similarity
+            MS2DeepScore similarity score.
+        """
+        binned_reference = self.model.spectrum_binner.transform([reference])[0]
+        binned_query = self.model.spectrum_binner.transform([query])[0]
+        reference_vectors = self.get_embedding_ensemble(binned_reference)
+        query_vectors = self.get_embedding_ensemble(binned_query)
+        scores_ensemble = cosine_similarity_matrix(reference_vectors, query_vectors)
+        
+        return scores_ensemble.mean(), scores_ensemble.std()
+
+    def matrix(self, references: List[Spectrum], queries: List[Spectrum],
+               is_symmetric: bool = False) -> np.ndarray:
+        """Calculate the MS2DeepScore similarities between all references and queries.
+
+        Parameters
+        ----------
+        references:
+            Reference spectrum.
+        queries:
+            Query spectrum.
+        is_symmetric:
+            Set to True if references == queries to speed up calculation about 2x.
+            Uses the fact that in this case score[i, j] = score[j, i]. Default is False.
+
+        Returns
+        -------
+        ms2ds_similarity
+            Array of MS2DeepScore similarity scores.
+        """
+        reference_vectors = self.calculate_vectors(references)
+        if is_symmetric:
+            assert np.all(references == queries), \
+                "Expected references to be equal to queries for is_symmetric=True"
+            query_vectors = reference_vectors
+        else:
+            query_vectors = self.calculate_vectors(queries)
+
+        ms2ds_similarity = cosine_similarity_matrix(reference_vectors, query_vectors)
+        return mean_pooling(ms2ds_similarity, self.n_ensembles), std_pooling(ms2ds_similarity, self.n_ensembles)
+
+    def calculate_vectors(self, spectrum_list: List[Spectrum]) -> np.ndarray:
+        """Returns a list of vectors for all spectra
+
+        parameters
+        ----------
+        spectrum_list:
+            List of spectra for which the vector should be calculated
+        """
+        n_rows = len(spectrum_list) * self.n_ensembles
+        reference_vectors = np.empty((n_rows, self.output_vector_dim), dtype="float")
+        binned_spectrums = self.model.spectrum_binner.transform(spectrum_list,
+                                                                progress_bar=self.progress_bar)
+        for index_ref, reference in enumerate(
+                tqdm(binned_spectrums,
+                     desc='Calculating vectors of reference spectrums',
+                     disable=(not self.progress_bar))):
+            embeddings = self.get_embedding_ensemble(reference)
+            reference_vectors[index_ref * self.n_ensembles:(index_ref + 1) * self.n_ensembles, 
+                              0:self.output_vector_dim] = embeddings
+        return reference_vectors
+    
+    def get_embedding_ensemble(self, spectrum_binned):
+        input_vector_array = np.tile(self._create_input_vector(spectrum_binned), (self.n_ensembles, 1))
+        return self.partial_model(input_vector_array, training=True)
---
+++ tests/test_ms2deepscoremontecarlo.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import numpy as np
+import pytest
+
+from ms2deepscore import MS2DeepScoreMonteCarlo
+from ms2deepscore.models import load_model
+from tests.test_user_worfklow import load_processed_spectrums
+
+TEST_RESOURCES_PATH = Path(__file__).parent / 'resources'
+
+
+def get_test_ms2_deep_score_instance(n_ensemble):
+    """Load data and models for MS2DeepScore unit tests."""
+    spectrums = load_processed_spectrums()
+
+    # Load pretrained model
+    model_file = TEST_RESOURCES_PATH / "testmodel.hdf5"
+    model = load_model(model_file)
+
+    similarity_measure = MS2DeepScoreMonteCarlo(model, n_ensemble)
+    return spectrums, model, similarity_measure
+
+
+def test_MS2DeepScoreMonteCarlo_vector_creation():
+    """Test vector creation.
+    """
+    spectrums, model, similarity_measure = get_test_ms2_deep_score_instance(n_ensembles=5)
+    binned_spectrum0 = model.spectrum_binner.transform([spectrums[0]])[0]
+    input_vectors = similarity_measure._create_input_vector(binned_spectrum0)
+    embeddings = similarity_measure.calculate_vectors([spectrums[0]])
+    assert input_vectors.shape == (1, 339), "Expected different vector shape"
+    assert embeddings.shape == (5, 200), "Expected different embeddings array shape"
+    assert isinstance(input_vectors, np.ndarray), "Expected vector to be numpy array"
+    assert isinstance(embeddings, np.ndarray), "Expected embeddings to be numpy array"
+    assert input_vectors[0, 92] == 1, "Expected different entries"
+
+
+def test_MS2DeepScoreMonteCarlo_score_pair():
+    """Test score calculation using *.pair* method."""
+    spectrums, _, similarity_measure = get_test_ms2_deep_score_instance(n_ensembles=5)
+    score, score_std = similarity_measure.pair(spectrums[0], spectrums[1])
+    assert isinstance(score, np.float64), "Expected float as score."
+    assert score > 0.2 and score < 0.5, "Expected score in different range"
+    assert isinstance(score_std, np.float64), "Expected float as STD."
+    assert score_std > 0.05 and score_std < 0.1, "Expected STD(score) in different range"
