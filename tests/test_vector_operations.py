be686ef4fbee7f759667d9760a2c9293254ec103
florian-huber <36473328+florian-huber@users.noreply.github.com>
2021-03-08 12:27:58 +0100

    add new tests

--- tests/test_vector_operations.py
+++ tests/test_vector_operations.py
@@ -1,16 +1,14 @@
-import os
-import numpy
+import numpy as np
 import pytest
-from matchms import Spectrum
-from ms2deepscore.vector_operations import cosine_similarity
-from ms2deepscore.vector_operations import cosine_similarity_matrix
+from ms2deepscore.vector_operations import cosine_similarity, cosine_similarity_matrix
+from ms2deepscore.vector_operations import mean_pooling, std_pooling
 
 
 @pytest.mark.parametrize("numba_compiled", [True, False])
 def test_cosine_similarity(numba_compiled):
     """Test cosine similarity score calculation."""
-    vector1 = numpy.array([1, 1, 0, 0])
-    vector2 = numpy.array([1, 1, 1, 1])
+    vector1 = np.array([1, 1, 0, 0])
+    vector2 = np.array([1, 1, 1, 1])
 
     if numba_compiled:
         score11 = cosine_similarity(vector1, vector1)
@@ -21,15 +19,15 @@ def test_cosine_similarity(numba_compiled):
         score12 = cosine_similarity.py_func(vector1, vector2)
         score22 = cosine_similarity.py_func(vector2, vector2)
 
-    assert score12 == 2 / numpy.sqrt(2 * 4), "Expected different score."
+    assert score12 == 2 / np.sqrt(2 * 4), "Expected different score."
     assert score11 == score22 == 1.0, "Expected different score."
 
 
 @pytest.mark.parametrize("numba_compiled", [True, False])
 def test_cosine_similarity_all_zeros(numba_compiled):
     """Test cosine similarity score calculation with empty vector."""
-    vector1 = numpy.array([0, 0, 0, 0])
-    vector2 = numpy.array([1, 1, 1, 1])
+    vector1 = np.array([0, 0, 0, 0])
+    vector2 = np.array([1, 1, 1, 1])
 
     if numba_compiled:
         score11 = cosine_similarity(vector1, vector1)
@@ -47,16 +45,16 @@ def test_cosine_similarity_all_zeros(numba_compiled):
 @pytest.mark.parametrize("numba_compiled", [True, False])
 def test_cosine_similarity_matrix(numba_compiled):
     """Test cosine similarity scores calculation using int32 input.."""
-    vectors1 = numpy.array([[1, 1, 0, 0],
-                            [1, 0, 1, 1]], dtype=numpy.int32)
-    vectors2 = numpy.array([[0, 1, 1, 0],
-                            [0, 0, 1, 1]], dtype=numpy.int32)
+    vectors1 = np.array([[1, 1, 0, 0],
+                         [1, 0, 1, 1]], dtype=np.int32)
+    vectors2 = np.array([[0, 1, 1, 0],
+                         [0, 0, 1, 1]], dtype=np.int32)
 
     if numba_compiled:
         scores = cosine_similarity_matrix(vectors1, vectors2)
     else:
         scores = cosine_similarity_matrix.py_func(vectors1, vectors2)
-    expected_scores = numpy.array([[0.5, 0.],
+    expected_scores = np.array([[0.5, 0.],
                                    [0.40824829, 0.81649658]])
     assert scores == pytest.approx(expected_scores, 1e-7), "Expected different scores."
 
@@ -64,44 +62,75 @@ def test_cosine_similarity_matrix(numba_compiled):
 @pytest.mark.parametrize("numba_compiled", [True, False])
 def test_cosine_similarity_floats_matrix(numba_compiled):
     """Test cosine similarity scores calculation using float64 input.."""
-    vectors1 = numpy.array([[1, 1, 0, 0],
-                            [1, 0, 1, 1]], dtype=numpy.float64)
-    vectors2 = numpy.array([[0, 1, 1, 0],
-                            [0, 0, 1, 1]], dtype=numpy.float64)
+    vectors1 = np.array([[1, 1, 0, 0],
+                         [1, 0, 1, 1]], dtype=np.float64)
+    vectors2 = np.array([[0, 1, 1, 0],
+                         [0, 0, 1, 1]], dtype=np.float64)
 
     if numba_compiled:
         scores = cosine_similarity_matrix(vectors1, vectors2)
     else:
         scores = cosine_similarity_matrix.py_func(vectors1, vectors2)
-    expected_scores = numpy.array([[0.5, 0.],
-                                   [0.40824829, 0.81649658]])
+    expected_scores = np.array([[0.5, 0.],
+                                [0.40824829, 0.81649658]])
     assert scores == pytest.approx(expected_scores, 1e-7), "Expected different scores."
 
 
 @pytest.mark.parametrize("numba_compiled", [True, False])
 def test_cosine_similarity_matrix_input_cloned(numba_compiled):
     """Test if score implementation clones the input correctly."""
-    vectors1 = numpy.array([[2, 2, 0, 0],
-                            [2, 0, 2, 2]])
-    vectors2 = numpy.array([[0, 2, 2, 0],
-                            [0, 0, 2, 2]])
+    vectors1 = np.array([[2, 2, 0, 0],
+                         [2, 0, 2, 2]])
+    vectors2 = np.array([[0, 2, 2, 0],
+                         [0, 0, 2, 2]])
 
     if numba_compiled:
         cosine_similarity_matrix(vectors1, vectors2)
     else:
         cosine_similarity_matrix.py_func(vectors1, vectors2)
 
-    assert numpy.all(vectors1 == numpy.array([[2, 2, 0, 0],
+    assert np.all(vectors1 == np.array([[2, 2, 0, 0],
                                               [2, 0, 2, 2]])), "Expected unchanged input."
 
 
 def test_different_input_vector_lengths():
     """Test if correct error is raised."""
-    vector1 = numpy.array([0, 0, 0, 0])
-    vector2 = numpy.array([1, 1, 1, 1, 1])
+    vector1 = np.array([0, 0, 0, 0])
+    vector2 = np.array([1, 1, 1, 1, 1])
 
     with pytest.raises(AssertionError) as msg:
         _ = cosine_similarity(vector1, vector2)
 
     expected_message = "Input vector must have same shape."
     assert expected_message == str(msg.value), "Expected particular error message."
+
+
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_mean_pooling(numba_compiled):
+    """Test if scores are pooled correctly."""
+    scores = np.arange(0, 16).reshape(4, 4)
+
+    if numba_compiled:
+        scores_mean = mean_pooling(scores, 2)
+    else:
+        scores_mean = mean_pooling.py_func(scores, 2)
+
+    scores_expected = np.array([[ 2.5,  4.5],
+                                [10.5, 12.5]])
+    assert np.allclose(scores_mean, scores_expected, atol=1e-8), \
+        "Expected different pooled mean scores"
+
+
+@pytest.mark.parametrize("numba_compiled", [True, False])
+def test_std_pooling(numba_compiled):
+    """Test if scores are pooled correctly."""
+    scores = np.arange(0, 16).reshape(4, 4)
+
+    if numba_compiled:
+        scores_std = std_pooling(scores, 2)
+    else:
+        scores_std = std_pooling.py_func(scores, 2)
+
+    std_expected = np.std([0,1,4,5]) * np.ones((2, 2))
+    assert np.allclose(scores_std, std_expected, atol=1e-8), \
+        "Expected different pooled standard deviations"
