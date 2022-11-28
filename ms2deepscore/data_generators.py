d60c7ed22b91be37982d8a4cf519835afab16b6e
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2022-11-28 14:26:33 +0100

    Apply suggestions from code review

--- ms2deepscore/data_generators.py
+++ ms2deepscore/data_generators.py
@@ -5,7 +5,7 @@ from typing import List, Iterator, NamedTuple
 
 import numpy as np
 import pandas as pd
-from tensorflow.keras.utils import Sequence
+from tensorflow.keras.utils import Sequence  # pylint: disable=import-error
 
 from .typing import BinnedSpectrumType
 
