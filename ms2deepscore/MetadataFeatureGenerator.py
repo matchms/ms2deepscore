6bfe0eadcc51b6b0f650f871ff084523c3f0224a
niekdejonge <niekdejonge@gmail.com>
2023-03-22 11:36:48 +0100

    Added MetadataFeatureGenerator class

---
+++ ms2deepscore/MetadataFeatureGenerator.py
@@ -0,0 +1,27 @@
+from matchms import Metadata
+
+
+class MetadataFeatureGenerator:
+    def __init__(self, metadata: Metadata):
+        self.metadata = metadata
+
+    def generate_features(self) -> float:
+        """This method should be implemented by child classes to generate a input feature for the model"""
+        raise NotImplementedError
+
+
+class PrecursorMZFeatureGenerator(MetadataFeatureGenerator):
+    def generate_features(self):
+        precursor_mz = self.metadata.get("precursor_mz")
+        assert precursor_mz is not None, "No precursor mz was found, preprocess your spectra first using matchms"
+        return precursor_mz//1000
+
+
+class IonizationModeFeatureGenerator(MetadataFeatureGenerator):
+    def generate_features(self):
+        ionization_mode = self.metadata.get("ionization_mode")
+        if ionization_mode == "positive":
+            return 0
+        if ionization_mode == "negative":
+            return 1
+        assert False, "Ionization mode should be 'positive' or 'negative'"
