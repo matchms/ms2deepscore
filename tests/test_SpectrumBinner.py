51f72e44ce9ec11228236c1be0ceab314f6fda84
niekdejonge <niekdejonge@gmail.com>
2023-03-24 15:36:42 +0100

    Update test spectrumbinner

--- tests/test_SpectrumBinner.py
+++ tests/test_SpectrumBinner.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from matchms import Spectrum
 from ms2deepscore import SpectrumBinner
-from ms2deepscore.MetadataFeatureGenerator import PrecursorMZFeatureGenerator, IonizationModeFeatureGenerator
+from ms2deepscore.MetadataFeatureGenerator import FeatureToBinary, StandardScaler
 
 def test_SpectrumBinner():
     """Test if default initalization works"""
@@ -119,11 +119,13 @@ def test_SpectrumBinner_transform_missing_fraction():
 
 def test_spectrum_binner_additional_metadata():
     ms2ds_binner = SpectrumBinner(100, mz_min=0.0, mz_max=100.0, peak_scaling=1.0,
-                                  additional_metadata=(PrecursorMZFeatureGenerator, IonizationModeFeatureGenerator))
+                                  additional_metadata=(StandardScaler("precursor_mz", mean=0, std=1000),
+                                                       FeatureToBinary("ionization_mode", "positive", "negative")))
     
     spectrum_1 = Spectrum(mz=np.array([10, 20, 50, 100.]),
                           intensities=np.array([0.7, 0.6, 0.2, 0.1]),
-                          metadata={'inchikey': "test_inchikey_01", "parent_mass": "100", "precursor_mz": "99", "ionization_mode": "positive"})
+                          metadata={'inchikey': "test_inchikey_01", "parent_mass": "100", "precursor_mz": "99",
+                                    "ionization_mode": "positive"})
     binned_spectrums = ms2ds_binner.fit_transform([spectrum_1])
     assert len(binned_spectrums[0].metadata) == 3, "Expected 3 items but found " + str(len(binned_spectrums[0].metadata)) + " in metadata."
     
