5c63c47afbdc5a41ab6e740c9da648c8b026798e
niekdejonge <niekdejonge@gmail.com>
2023-04-17 14:09:11 +0200

    Use save and load pickled file from utils in tanimoto scores

--- notebooks/train_with_additional_metadata/train_ms2deepscore/calculate_tanimoto_scores.py
+++ notebooks/train_with_additional_metadata/train_ms2deepscore/calculate_tanimoto_scores.py
@@ -1,18 +1,15 @@
+import os
+from ms2deepscore.utils import load_pickled_file, save_pickled_file, return_non_existing_file_name
 from ms2deepscore.train_new_model.calculate_tanimoto_matrix import calculate_tanimoto_scores_unique_inchikey
 
 if __name__ == "__main__":
-    import pickle
-    import os
-    def load_pickled_file(filename: str):
-        with open(filename, 'rb') as file:
-            loaded_object = pickle.load(file)
-        return loaded_object
-    all_spectra = load_pickled_file("../../../../data/cleaned_spectra/negative_annotated_spectra.pickle")
+
+    postive_spectra = load_pickled_file("../../../../data/training_and_validation_split/positive_validation_spectra.pickle")
+    negative_spectra = load_pickled_file("../../../../data/training_and_validation_split/negative_validation_spectra.pickle")
+
+    all_spectra = postive_spectra + negative_spectra
+
     print("loaded in spectra")
     tanimoto_scores = calculate_tanimoto_scores_unique_inchikey(all_spectra, all_spectra)
 
-    def save_pickled_file(obj, filename: str):
-        assert not os.path.exists(filename), "File already exists"
-        with open(filename, "wb") as f:
-            pickle.dump(obj, f)
-    save_pickled_file(tanimoto_scores, "../../../../data/tanimoto_scores/negative_tanimoto_scores.pickle")
+    save_pickled_file(tanimoto_scores, "../../../../data/tanimoto_scores/validation_tanimoto_scores.pickle")
