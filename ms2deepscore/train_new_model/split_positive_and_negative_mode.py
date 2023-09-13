a63ec0f6f745f29a7e3e26d184ded7fe955c1f82
niekdejonge <niekdejonge@gmail.com>
2023-09-13 16:38:05 +0200

    Add split pos and neg mode

---
+++ ms2deepscore/train_new_model/split_positive_and_negative_mode.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from matchms import Spectrum
+from tqdm import tqdm
+
+
+def split_pos_and_neg(spectra: List[Spectrum]):
+    """Removes spectra that are not in the correct ionmode"""
+    pos_spectra = []
+    neg_spectra = []
+    spectra_removed = 0
+    for spectrum in tqdm(spectra,
+                         desc=f"Splitting pos and neg mode spectra"):
+        if spectrum is not None:
+            ionmode = spectrum.get("ionmode")
+            if ionmode == "positive":
+                pos_spectra.append(spectrum)
+            elif ionmode == "negative":
+                neg_spectra.append(spectrum)
+            else:
+                spectra_removed += 1
+    print(f"The spectra, are split in {len(pos_spectra)} positive spectra "
+          f"and {len(neg_spectra)} negative mode spectra. {spectra_removed} were removed")
+    return pos_spectra, neg_spectra
