debcfecd43a54b954efb632a732445d3e94da80a
niekdejonge <niekdejonge@gmail.com>
2023-10-16 15:18:26 +0200

    Fix SettingMS2Deepscore mistake with trailing commas and use hasattr and setattr

--- ms2deepscore/train_new_model/SettingMS2Deepscore.py
+++ ms2deepscore/train_new_model/SettingMS2Deepscore.py
@@ -1,15 +1,15 @@
 class SettingsMS2Deepscore:
     def __init__(self, settings=None):
-        self.epochs = 150,
-        self.base_dims = (500, 500),
-        self.embedding_dim = 200,
-        self.average_pairs_per_bin = 20,
+        self.epochs = 150
+        self.base_dims = (500, 500)
+        self.embedding_dim = 200
+        self.average_pairs_per_bin = 20
         self.max_pairs_per_bin = 100
         self.additional_metadata = ()
 
         if settings:
             for key, value in settings.items():
-                if key in self.__dict__:
-                    self.__dict__[key] = value
+                if hasattr(self, key):
+                    setattr(self, key, value)
                 else:
                     raise ValueError(f"Unknown setting: {key}")
