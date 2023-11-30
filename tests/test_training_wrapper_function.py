29dc5da3fa581ee86851b4d13bbaeffe4af9134f
niekdejonge <niekdejonge@gmail.com>
2023-11-30 14:41:42 +0100

    Fix test_train_wrapper_ms2ds_model by setting the batch size lower

--- tests/test_training_wrapper_function.py
+++ tests/test_training_wrapper_function.py
@@ -3,16 +3,10 @@ import pickle
 from pathlib import Path
 from matchms.exporting import save_as_mgf
 from matchms.importing import load_from_mgf
-from ms2deepscore.models import SiameseModel
-from ms2deepscore.models.load_model import \
-    load_model as load_ms2deepscore_model
 from ms2deepscore.train_new_model.SettingMS2Deepscore import \
     SettingsMS2Deepscore
-from ms2deepscore.train_new_model.train_ms2deepscore import (bin_spectra,
-                                                             train_ms2ds_model)
 from ms2deepscore.train_new_model.training_wrapper_functions import \
     train_ms2deepscore_wrapper
-
 
 TEST_RESOURCES_PATH = Path(__file__).parent / 'resources'
 
@@ -24,10 +18,11 @@ def load_pickled_file(filename: str):
 
 
 def test_train_wrapper_ms2ds_model(tmp_path):
-    # dir = os.path.join(TEST_RESOURCES_PATH, "test_wrapper")
     spectra = list(load_from_mgf(os.path.join(TEST_RESOURCES_PATH, "pesticides_processed.mgf")))
     save_as_mgf(spectra, filename=os.path.join(tmp_path, "clean_spectra.mgf"))
     settings = SettingsMS2Deepscore({"epochs": 2,
                                      "average_pairs_per_bin": 2,
-                                     "ionisation_mode": "negative"})
+                                     "ionisation_mode": "negative",
+                                     "batch_size": 2})
     train_ms2deepscore_wrapper(tmp_path, "clean_spectra.mgf", settings, 20)
+    assert os.path.isfile(os.path.join(tmp_path, "trained_models", settings.model_directory_name, ""))
