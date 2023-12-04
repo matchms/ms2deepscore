e8c08ee4286486c44bd1b7adab208021882ea3af
niekdejonge <niekdejonge@gmail.com>
2023-12-04 17:20:54 +0100

    Implement BenchmarkingResultsFileNames

--- ms2deepscore/wrapper_functions/plotting_wrapper_functions.py
+++ ms2deepscore/wrapper_functions/plotting_wrapper_functions.py
@@ -13,29 +13,40 @@ from ms2deepscore.wrapper_functions.StoreTrainingData import StoreTrainingData
 from ms2deepscore.train_new_model.SettingMS2Deepscore import SettingsMS2Deepscore
 
 
+class BenchmarkingResultsFileNames:
+    def __init__(self, model_folder, file_name_prefix):
+        self.file_name_prefix = file_name_prefix
+        self.benchmarking_results_folder = os.path.join(model_folder, "benchmarking_results")
+        os.makedirs(self.benchmarking_results_folder, exist_ok=True)
+        self.true_values_file_name = os.path.join(self.benchmarking_results_folder, f"{file_name_prefix}_true_values.pickle")
+        self.predictions_file_name = os.path.join(self.benchmarking_results_folder, f"{file_name_prefix}_predictions.pickle")
+
+        self.plots_folder = os.path.join(self.benchmarking_results_folder, "plots")
+        os.makedirs(self.plots_folder, exist_ok=True)
+
+        self.normal_plot_file_name = os.path.join(self.plots_folder, f"{file_name_prefix}_plot.svg")
+        self.reversed_plot_file_name = os.path.join(self.plots_folder, f"{file_name_prefix}_reversed_plot.svg")
+        self.averages_summary_file_name = os.path.join(self.benchmarking_results_folder, "RMSE_and_MAE.txt")
+
+
 def benchmark_wrapper(val_spectra_1: List[Spectrum],
                       val_spectra_2: List[Spectrum],
-                      benchmarking_results_folder: str,
-                      ms2ds_model: MS2DeepScore,
-                      file_name_prefix):
+                      benchmarking_results_file_names: BenchmarkingResultsFileNames,
+                      ms2ds_model: MS2DeepScore):
     true_values = create_or_load_true_values(val_spectra_1, val_spectra_2,
-                                             benchmarking_results_folder,
-                                             file_name_prefix)
+                                             benchmarking_results_file_names.true_values_file_name)
     predictions = create_or_load_predicted_values(val_spectra_1, val_spectra_2,
-                                                  benchmarking_results_folder,
-                                                  ms2ds_model,
-                                                  file_name_prefix)
-    create_all_plots(predictions, true_values, benchmarking_results_folder, file_name_prefix)
+                                                  benchmarking_results_file_names.predictions_file_name,
+                                                  ms2ds_model)
+    create_all_plots(predictions, true_values, benchmarking_results_file_names)
 
 
 def create_or_load_true_values(val_spectra_1: List[Spectrum],
                                val_spectra_2: List[Spectrum],
-                               benchmarking_results_folder: str,
-                               file_name_prefix):
-    true_values_file_name = os.path.join(benchmarking_results_folder, f"{file_name_prefix}_true_values.pickle")
+                               true_values_file_name):
     if os.path.exists(true_values_file_name):
         true_values = load_pickled_file(true_values_file_name)
-        print(f"Loaded in previous true values for: {file_name_prefix}")
+        print(f"Loaded in previous true values from: {true_values_file_name}")
     else:
         # Calculate true values
         true_values = get_tanimoto_score_between_spectra(val_spectra_1, val_spectra_2)
@@ -45,13 +56,11 @@ def create_or_load_true_values(val_spectra_1: List[Spectrum],
 
 def create_or_load_predicted_values(val_spectra_1: List[Spectrum],
                                     val_spectra_2: List[Spectrum],
-                                    benchmarking_results_folder: str,
-                                    ms2ds_model: MS2DeepScore,
-                                    file_name_prefix):
-    predictions_file_name = os.path.join(benchmarking_results_folder, f"{file_name_prefix}_predictions.pickle")
+                                    predictions_file_name: str,
+                                    ms2ds_model: MS2DeepScore):
     if os.path.exists(predictions_file_name):
         predictions = load_pickled_file(predictions_file_name)
-        print(f"Loaded in previous predictions for: {file_name_prefix}")
+        print(f"Loaded in previous predictions from {predictions_file_name}")
     else:
         is_symmetric = (val_spectra_1 == val_spectra_2)
         predictions = ms2ds_model.matrix(val_spectra_1, val_spectra_2, is_symmetric=is_symmetric)
@@ -61,33 +70,26 @@ def create_or_load_predicted_values(val_spectra_1: List[Spectrum],
 
 def create_all_plots(predictions,
                      true_values,
-                     benchmarking_results_folder: str,
-                     file_name_prefix):
+                     benchmarking_results_file_names: BenchmarkingResultsFileNames):
     """Creates and saves plots and in between files for validation spectra
 
     Arguments:
         predictions: The predictions made by the model
         true_values: The true values predicted by the model
-        benchmarking_results_folder: The results folder for the benchmarking
-        ms2ds_model: The loaded ms2deepscore model
-        file_name_prefix: The prefix used to create all file names.
+        benchmarking_results_file_names: Class storing all the default file names and folder structure
     """
-    plots_folder = os.path.join(benchmarking_results_folder, "plots_normalized_auc")
-    os.makedirs(plots_folder, exist_ok=True)
-
     # Create plots
     plot_histograms(predictions, true_values, 10, 100)
-    plt.savefig(os.path.join(plots_folder, f"{file_name_prefix}_plot.svg"))
+    plt.savefig(benchmarking_results_file_names.normal_plot_file_name)
     # Create reverse plot
     plot_histograms(true_values, predictions, 10, 100)
-    plt.savefig(os.path.join(plots_folder, f"{file_name_prefix}_reversed_plot.svg"))
+    plt.savefig(benchmarking_results_file_names.reversed_plot_file_name)
 
     mae = np.abs(predictions - true_values).mean()
     rmse = np.sqrt(np.square(predictions - true_values).mean())
-    summary = f"For {file_name_prefix} the mae={mae} and rmse={rmse}\n"
+    summary = f"For {benchmarking_results_file_names.file_name_prefix} the mae={mae} and rmse={rmse}\n"
 
-    averages_summary_file = os.path.join(benchmarking_results_folder, "RMSE_and_MAE.txt")
-    with open(averages_summary_file, "a", encoding="utf-8") as f:
+    with open(benchmarking_results_file_names.averages_summary_file_name, "a", encoding="utf-8") as f:
         f.write(summary)
     print(summary)
 
@@ -106,16 +108,16 @@ def create_all_plots_wrapper(training_data_storing: StoreTrainingData,
     # Load in MS2Deepscore model
     ms2deepscore_model = MS2DeepScore(load_model(os.path.join(model_folder, settings.model_file_name)))
 
-    # Create benchmarking results folder
-    benchmarking_results_folder = os.path.join(model_folder, "benchmarking_results")
-    os.makedirs(benchmarking_results_folder, exist_ok=True)
-
-    benchmark_wrapper(positive_validation_spectra, positive_validation_spectra, benchmarking_results_folder,
-                      ms2deepscore_model, "positive_positive")
-    benchmark_wrapper(negative_validation_spectra, negative_validation_spectra, benchmarking_results_folder,
-                      ms2deepscore_model, "negative_negative")
-    benchmark_wrapper(negative_validation_spectra, positive_validation_spectra, benchmarking_results_folder,
-                      ms2deepscore_model, "negative_positive")
+    benchmark_wrapper(positive_validation_spectra, positive_validation_spectra,
+                      BenchmarkingResultsFileNames(model_folder, "positive_positive"),
+                      ms2deepscore_model)
+    benchmark_wrapper(negative_validation_spectra, negative_validation_spectra,
+                      BenchmarkingResultsFileNames(model_folder, "negative_negative"),
+                      ms2deepscore_model, )
+    benchmark_wrapper(negative_validation_spectra, positive_validation_spectra,
+                      BenchmarkingResultsFileNames(model_folder, "negative_positive"),
+                      ms2deepscore_model, )
     benchmark_wrapper(positive_validation_spectra + negative_validation_spectra,
-                      positive_validation_spectra + negative_validation_spectra, benchmarking_results_folder,
-                      ms2deepscore_model, "both_both")
+                      positive_validation_spectra + negative_validation_spectra,
+                      BenchmarkingResultsFileNames(model_folder, "both_both"),
+                      ms2deepscore_model, )
