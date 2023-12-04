4926f65295a715a6143708fa2397a76bb701f101
niekdejonge <niekdejonge@gmail.com>
2023-12-04 16:39:36 +0100

    Split create all plots and create_true_and_predicted_values

--- ms2deepscore/visualize_results/plotting_wrapper_functions.py
+++ ms2deepscore/visualize_results/plotting_wrapper_functions.py
@@ -1,36 +1,31 @@
 import os
 from typing import List
 import numpy as np
-import pandas as pd
 from matchms import Spectrum
-from matchms.similarity.vector_similarity_functions import jaccard_similarity_matrix
 from matplotlib import pyplot as plt
-from rdkit import Chem
-from tqdm import tqdm
 
 from ms2deepscore import MS2DeepScore
-from ms2deepscore.train_new_model.spectrum_pair_selection import select_inchi_for_unique_inchikeys
 from ms2deepscore.utils import load_pickled_file, save_pickled_file
 from ms2deepscore.visualize_results.calculate_tanimoto_scores import get_tanimoto_score_between_spectra
 from ms2deepscore.visualize_results.plotting import plot_histograms
 
 
 def benchmark_wrapper(val_spectra_1: List[Spectrum],
-                      val_spectra_2: List[Spectrum],
-                      benchmarking_results_folder: str,
-                      ms2ds_model: MS2DeepScore,
-                      file_name_prefix):
-    """Creates and saves plots and in between files for validation spectra
+                                     val_spectra_2: List[Spectrum],
+                                     benchmarking_results_folder: str,
+                                     ms2ds_model: MS2DeepScore,
+                                     file_name_prefix):
+    true_values, predictions =create_true_and_predicted_values(val_spectra_1,val_spectra_2,
+                                     benchmarking_results_folder, ms2ds_model,
+                                     file_name_prefix)
+    create_all_plots(predictions, true_values, benchmarking_results_folder, file_name_prefix)
 
-    Arguments:
-        val_spectra_1: The first validation spectra
-        val_spectra_2: The second validation spectra
-        benchmarking_results_folder: The results folder for the benchmarking
-        ms2ds_model: The loaded ms2deepscore model
-        file_name_prefix: The prefix used to create all file names.
-    """
-    # pylint: disable=too-many-locals
-    # Create predictions
+
+def create_true_and_predicted_values(val_spectra_1: List[Spectrum],
+                                     val_spectra_2: List[Spectrum],
+                                     benchmarking_results_folder: str,
+                                     ms2ds_model: MS2DeepScore,
+                                     file_name_prefix):
     predictions_file_name = os.path.join(benchmarking_results_folder, f"{file_name_prefix}_predictions.pickle")
     if os.path.exists(predictions_file_name):
         predictions = load_pickled_file(predictions_file_name)
@@ -48,7 +43,22 @@ def benchmark_wrapper(val_spectra_1: List[Spectrum],
     else:
         true_values = get_tanimoto_score_between_spectra(val_spectra_1, val_spectra_2)
         save_pickled_file(true_values, true_values_file_name)
+    return true_values, predictions
 
+
+def create_all_plots(predictions,
+                     true_values,
+                     benchmarking_results_folder: str,
+                     file_name_prefix):
+    """Creates and saves plots and in between files for validation spectra
+
+    Arguments:
+        val_spectra_1: The first validation spectra
+        val_spectra_2: The second validation spectra
+        benchmarking_results_folder: The results folder for the benchmarking
+        ms2ds_model: The loaded ms2deepscore model
+        file_name_prefix: The prefix used to create all file names.
+    """
     plots_folder = os.path.join(benchmarking_results_folder, "plots_normalized_auc")
     os.makedirs(plots_folder, exist_ok=True)
 
