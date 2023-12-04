9a3cbc9c51043c0b9f55bc7e3f8e686e4b213903
niekdejonge <niekdejonge@gmail.com>
2023-12-04 17:44:39 +0100

    Add test benchmark_wrapper_not_symmetric

--- tests/test_plotting_wrapper_functions.py
+++ tests/test_plotting_wrapper_functions.py
@@ -6,3 +6,12 @@ def test_benchmark_wrapper(tmp_path):
     spectra = pesticides_test_spectra()
     benchmarking_results_file_names = BenchmarkingResultsFileNames(tmp_path, "negative_negative")
     benchmark_wrapper(spectra, spectra, benchmarking_results_file_names, ms2deepscore_model())
+
+
+def test_benchmark_wrapper_not_symmetric(tmp_path):
+    spectra = pesticides_test_spectra()
+    positive_mode_spectra = [spectrum.set("ionmode", "positive") for spectrum in spectra[:20]]
+    negative_mode_spectra = [spectrum.set("ionmode", "negative") for spectrum in spectra[20:]]
+    benchmarking_results_file_names = BenchmarkingResultsFileNames(tmp_path, "positive_negative")
+    benchmark_wrapper(positive_mode_spectra, negative_mode_spectra,
+                      benchmarking_results_file_names, ms2deepscore_model())
