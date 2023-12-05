95e3815bae8e42e0d6d5a17734654049dd9be758
niekdejonge <niekdejonge@gmail.com>
2023-12-05 08:54:37 +0100

    Add checks that files are actually created by wrapper

--- tests/test_training_wrapper_function.py
+++ tests/test_training_wrapper_function.py
@@ -8,6 +8,7 @@ from ms2deepscore.train_new_model.SettingMS2Deepscore import \
     SettingsMS2Deepscore
 from ms2deepscore.wrapper_functions.training_wrapper_functions import \
     train_ms2deepscore_wrapper
+from ms2deepscore.wrapper_functions.StoreTrainingData import StoreTrainingData
 
 
 def load_pickled_file(filename: str):
@@ -18,14 +19,28 @@ def load_pickled_file(filename: str):
 
 def test_train_wrapper_ms2ds_model(tmp_path):
     spectra = pesticides_test_spectra()
-    positive_mode_spectra = [spectrum.set("ionmode", "positive") for spectrum in spectra[:20]]
-    negative_mode_spectra = [spectrum.set("ionmode", "negative") for spectrum in spectra[20:]]
+    positive_mode_spectra = [spectrum.set("ionmode", "positive") for spectrum in spectra[:35]]
+    negative_mode_spectra = [spectrum.set("ionmode", "negative") for spectrum in spectra[35:]]
 
-    save_as_mgf(positive_mode_spectra+negative_mode_spectra, filename=os.path.join(tmp_path, "clean_spectra.mgf"))
+    save_as_mgf(positive_mode_spectra+negative_mode_spectra,
+                filename=os.path.join(tmp_path, "clean_spectra.mgf"))
     settings = SettingsMS2Deepscore({"epochs": 2,
                                      "average_pairs_per_bin": 2,
                                      "ionisation_mode": "negative",
                                      "batch_size": 2})
     train_ms2deepscore_wrapper(tmp_path, "clean_spectra.mgf", settings, 5)
-    assert os.path.isfile(os.path.join(tmp_path, "trained_models", settings.model_directory_name,
-                                       "ms2deepscore_model.hdf5"))
+    expected_file_names = StoreTrainingData(tmp_path, "clean_spectra.mgf")
+    assert os.path.isfile(os.path.join(tmp_path, expected_file_names.trained_models_folder,
+                                       settings.model_directory_name, settings.model_file_name))
+    assert os.path.isfile(expected_file_names.negative_mode_spectra_file)
+    assert os.path.isfile(expected_file_names.negative_validation_spectra_file)
+    assert os.path.isfile(os.path.join(tmp_path, expected_file_names.trained_models_folder,
+                                       settings.model_directory_name, "benchmarking_results",
+                                       "both_both_predictions.pickle"))
+    assert os.path.isfile(os.path.join(tmp_path, expected_file_names.trained_models_folder,
+                                       settings.model_directory_name, "benchmarking_results",
+                                       "plots", "both_both_plot.svg"))
+    assert os.path.isfile(os.path.join(tmp_path, expected_file_names.trained_models_folder,
+                                       settings.model_directory_name, "binned_spectra",
+                                       "binned_training_spectra.pickle"))
+
