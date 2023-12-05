da07dbce27129f15ede42ef381d577611bc75084
niekdejonge <niekdejonge@gmail.com>
2023-12-05 11:02:42 +0100

    Add save as json to SettingMS2Deepscore.py

--- tests/test_SettingsMS2deepscore.py
+++ tests/test_SettingsMS2deepscore.py
@@ -1,5 +1,7 @@
+import json
+import os
 import pytest
-
+import json
 from ms2deepscore.train_new_model.SettingMS2Deepscore import \
     SettingsMS2Deepscore
 
@@ -15,3 +17,16 @@ def test_initiate_settingsms2deepscore():
 def test_set_unknown_settings():
     with pytest.raises(ValueError):
         SettingsMS2Deepscore({"test_case": 123})
+
+
+def test_save_settings(tmp_path):
+    settings = SettingsMS2Deepscore({"epochs": 200,
+                                     "base_dims": (200, 200)})
+    file_name = os.path.join(tmp_path, "settings.json")
+    settings.save_to_file(file_name)
+    assert os.path.isfile(file_name)
+    with open(file_name, 'r') as file:
+        result = json.load(file)
+    assert result["epochs"] == 200
+    assert result["base_dims"] == [200, 200]
+    assert result["embedding_dim"] == 200
