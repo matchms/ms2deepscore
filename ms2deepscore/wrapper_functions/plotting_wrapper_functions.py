4b4a925d1b9a899928a60520af98e6a1bcf54f6b
niekdejonge <niekdejonge@gmail.com>
2024-01-04 14:19:10 +0100

    Move function order

--- ms2deepscore/wrapper_functions/plotting_wrapper_functions.py
+++ ms2deepscore/wrapper_functions/plotting_wrapper_functions.py
@@ -8,13 +8,45 @@ from ms2deepscore.benchmarking_models.select_spectrum_pairs_for_visualization im
 from ms2deepscore.utils import load_pickled_file, load_spectra_as_list
 
 
+def create_plots_between_all_ionmodes(model_directory,
+                                      results_folder=None):
+    spectra_folder = os.path.join(model_directory, "..", "..", "training_and_validation_split")
+    if results_folder is None:
+        results_folder = os.path.join(model_directory, "benchmarking_results", "plots")
+    os.makedirs(results_folder, exist_ok=True)
+    positive_validation_spectra = load_spectra_as_list(os.path.join(spectra_folder, "positive_validation_spectra.mgf"))
+    negative_validation_spectra = load_spectra_as_list(os.path.join(spectra_folder, "negative_validation_spectra.mgf"))
+    both_validation_spectra = positive_validation_spectra + negative_validation_spectra
+
+    validation_spectra = {"positive": positive_validation_spectra,
+                          "negative": negative_validation_spectra,
+                          "both": both_validation_spectra}
+
+    possible_comparisons = (("positive", "positive"),
+                            ("negative", "positive"),
+                            ("negative", "negative"),
+                            ("both", "both"))
+
+    for ionmode_1, ionmode_2 in possible_comparisons:
+        create_all_plots(predictions=load_pickled_file(os.path.join(model_directory,
+                                                                    "benchmarking_results",
+                                                                    f"{ionmode_1}_{ionmode_2}_predictions.pickle")),
+                         true_values=load_pickled_file(os.path.join(model_directory,
+                                                                    "benchmarking_results",
+                                                                    f"{ionmode_1}_{ionmode_2}_true_values.pickle")),
+                         val_spectra_1=validation_spectra[ionmode_1],
+                         val_spectra_2=validation_spectra[ionmode_2],
+                         benchmarking_results_folder=results_folder,
+                         file_name_prefix=f"{ionmode_1}_vs_{ionmode_2}")
+
+
 def create_all_plots(predictions,
                      true_values,
                      val_spectra_1,
                      val_spectra_2,
                      benchmarking_results_folder,
                      file_name_prefix,
-                     ):
+                     ):  # pylint: disable=too-many-arguments
     """Creates and saves plots and in between files for validation spectra
 
     Arguments:
@@ -48,35 +80,3 @@ def create_all_plots(predictions,
     # with open(benchmarking_results_file_names.averages_summary_file_name, "a", encoding="utf-8") as f:
     #     f.write(summary)
     print(summary)
-
-
-def create_plots_between_all_ionmodes(model_directory,
-                                      results_folder=None):
-    spectra_folder = os.path.join(model_directory, "..", "..", "training_and_validation_split")
-    if results_folder is None:
-        results_folder = os.path.join(model_directory, "benchmarking_results", "plots")
-    os.makedirs(results_folder, exist_ok=True)
-    positive_validation_spectra = load_spectra_as_list(os.path.join(spectra_folder, "positive_validation_spectra.mgf"))
-    negative_validation_spectra = load_spectra_as_list(os.path.join(spectra_folder, "negative_validation_spectra.mgf"))
-    both_validation_spectra = positive_validation_spectra + negative_validation_spectra
-
-    validation_spectra = {"positive": positive_validation_spectra,
-                          "negative": negative_validation_spectra,
-                          "both": both_validation_spectra}
-
-    possible_comparisons = (("positive", "positive"),
-                            ("negative", "positive"),
-                            ("negative", "negative"),
-                            ("both", "both"))
-
-    for ionmode_1, ionmode_2 in possible_comparisons:
-        create_all_plots(predictions=load_pickled_file(os.path.join(model_directory,
-                                                                    "benchmarking_results",
-                                                                    f"{ionmode_1}_{ionmode_2}_predictions.pickle")),
-                         true_values=load_pickled_file(os.path.join(model_directory,
-                                                                    "benchmarking_results",
-                                                                    f"{ionmode_1}_{ionmode_2}_true_values.pickle")),
-                         val_spectra_1=validation_spectra[ionmode_1],
-                         val_spectra_2=validation_spectra[ionmode_2],
-                         benchmarking_results_folder=results_folder,
-                         file_name_prefix=f"{ionmode_1}_vs_{ionmode_2}")
