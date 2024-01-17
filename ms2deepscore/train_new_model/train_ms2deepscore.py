d70e88ce5d04ff2cba79b88bef53f61230c1e90a
niekdejonge <niekdejonge@gmail.com>
2024-01-17 11:51:28 +0100

    Integrate pytorch training in train ms2deepscore

--- ms2deepscore/train_new_model/train_ms2deepscore.py
+++ ms2deepscore/train_new_model/train_ms2deepscore.py
@@ -3,18 +3,17 @@ This script is not needed for normally running MS2Deepscore, it is only needed t
 """
 
 import os
-from typing import Dict, List, Optional
-import tensorflow as tf
+from typing import List, Optional
 from matchms import Spectrum
 from matplotlib import pyplot as plt
 from ms2deepscore import SpectrumBinner
-from ms2deepscore.data_generators import DataGeneratorCherrypicked
-from ms2deepscore.models import SiameseModel
 from ms2deepscore.train_new_model.SettingMS2Deepscore import \
     SettingsMS2Deepscore
 from ms2deepscore.train_new_model.spectrum_pair_selection import \
     select_compound_pairs_wrapper
 from ms2deepscore.utils import return_non_existing_file_name, save_pickled_file
+from ms2deepscore.data_generators import DataGeneratorPytorch
+from ms2deepscore.models.SiameseSpectralModel import SiameseSpectralModel, train
 
 
 def bin_spectra(
@@ -64,96 +63,61 @@ def train_ms2ds_model(
         settings: SettingsMS2Deepscore):
     """Full workflow to train a MS2DeepScore model.
     """
-    # pylint: disable=too-many-locals
     model_directory = os.path.join(results_folder, settings.model_directory_name)
     os.makedirs(model_directory, exist_ok=True)
     # Save settings
     settings.save_to_file(os.path.join(model_directory, "settings.json"))
 
     output_model_file_name = os.path.join(model_directory, settings.model_file_name)
-    ms2ds_history_file_name = os.path.join(model_directory, settings.history_file_name)
     ms2ds_history_plot_file_name = os.path.join(model_directory, settings.history_plot_file_name)
 
-    binned_spectra_folder = os.path.join(model_directory, settings.binned_spectra_folder_name)
-    os.makedirs(binned_spectra_folder, exist_ok=True)
+    # todo remove the binned spectra folder and bin spectra in training wrapper
 
     selected_compound_pairs_training, selected_training_spectra = select_compound_pairs_wrapper(
         training_spectra, settings=settings)
+
+    # todo check if num_turns=2 and augment_noise_max = 10 should still be added.
+    # Create generators
+    train_generator = DataGeneratorPytorch(
+        spectrums=selected_training_spectra,
+        min_mz=10, max_mz=1000, mz_bin_width=0.1, intensity_scaling=0.5,
+        metadata_vectorizer=None,
+        selected_compound_pairs=selected_compound_pairs_training,
+        augment_intensity=0.1,
+        batch_size=settings.batch_size,
+    )
     selected_compound_pair_val, selected_validation_spectra = select_compound_pairs_wrapper(
         validation_spectra, settings=settings)
-
-    # Created binned spectra.
-    binned_spectrums_training, binned_spectrums_val, spectrum_binner = \
-        bin_spectra(selected_training_spectra, selected_validation_spectra, settings.additional_metadata, binned_spectra_folder)
-
-    training_generator = DataGeneratorCherrypicked(
-        binned_spectrums_training,
-        selected_compound_pairs=selected_compound_pairs_training,
-        spectrum_binner=spectrum_binner,
-        num_turns=2,
-        augment_noise_max=10,
-        augment_noise_intensity=0.01,
-        batch_size=settings.batch_size
-    )
-
-    validation_generator = DataGeneratorCherrypicked(
-        binned_spectrums_val,
+    # todo maybe add num_turns=10 # Number of pairs for each InChiKey14 during each epoch. again?
+    val_generator = DataGeneratorPytorch(
+        spectrums=selected_validation_spectra,
+        min_mz=10, max_mz=1000, mz_bin_width=0.1, intensity_scaling=0.5,
+        metadata_vectorizer=None,
         selected_compound_pairs=selected_compound_pair_val,
-        spectrum_binner=spectrum_binner,
-        num_turns=10,  # Number of pairs for each InChiKey14 during each epoch.
-        # To prevent data augmentation
-        augment_removal_max=0,
-        augment_removal_intensity=0,
-        augment_intensity=0,
-        augment_noise_max=0,
+        batch_size=settings.batch_size,
         use_fixed_set=True,
-        batch_size=settings.batch_size
+        augment_removal_max=0.0,
+        augment_removal_intensity=0.0,
+        augment_intensity=0.0,
+        augment_noise_max=0
     )
+    # todo check if dropout rate, layers, base dim and embedding size should still be integrated.
+    # todo Check where we specify the loss type. Should that happen here as well, maybe settings?
+    model = SiameseSpectralModel(peak_inputs=train_generator.num_bins, additional_inputs=0,
+                                 train_binning_layer=False)
 
-    model = SiameseModel(
-        spectrum_binner,
-        base_dims=settings.base_dims,
-        embedding_dim=settings.embedding_dim,
-        dropout_rate=settings.dropout_rate,
-    )
-
-    model.compile(
-        loss="mse",
-        optimizer=tf.keras.optimizers.Adam(learning_rate=settings.learning_rate),
-        metrics=["mae", tf.keras.metrics.RootMeanSquaredError()],
-    )
-    # Save best model and include early stopping
-    checkpointer = tf.keras.callbacks.ModelCheckpoint(
-        filepath=output_model_file_name,
-        monitor="val_loss",
-        mode="min",
-        verbose=1,
-        save_best_only=True,
-    )
-    earlystopper_scoring_net = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss", mode="min", patience=settings.patience, verbose=1
-    )
-    # Fit model and save history
-    history = model.model.fit(
-        training_generator,
-        validation_data=validation_generator,
-        epochs=settings.epochs,
-        verbose=1,
-        callbacks=[checkpointer, earlystopper_scoring_net],
-    )
-    model.load_weights(output_model_file_name)
-    model.save(output_model_file_name)
-
-    # Save history
-    with open(ms2ds_history_file_name, "w", encoding="utf-8") as f:
-        f.write(str(history.history))
+    losses, val_losses, collection_targets = train(
+        model, train_generator, 200,
+        val_generator=val_generator,
+        checkpoint_filename=output_model_file_name,
+        learning_rate=settings.learning_rate, lambda_l1=0, lambda_l2=0, patience=settings.patience)
     # Save plot of history
-    plot_history(history.history, ms2ds_history_plot_file_name)
+    plot_history(losses, val_losses, ms2ds_history_plot_file_name)
 
 
-def plot_history(history: Dict[str, List[float]], file_name: Optional[str] = None):
-    plt.plot(history["loss"])
-    plt.plot(history["val_loss"])
+def plot_history(losses, val_losses, file_name: Optional[str] = None):
+    plt.plot(losses)
+    plt.plot(val_losses)
     plt.title("model loss")
     plt.ylabel("loss")
     plt.xlabel("epoch")
