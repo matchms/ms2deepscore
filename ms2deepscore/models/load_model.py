50cef2bd4c7d0d5397e9d0892fcc5c3307dbee10
niekdejonge <niekdejonge@gmail.com>
2024-01-23 14:22:35 +0100

    Check version when loading model

--- ms2deepscore/models/load_model.py
+++ ms2deepscore/models/load_model.py
@@ -3,6 +3,7 @@ from typing import Union
 import torch
 from ms2deepscore.SettingsMS2Deepscore import TensorizationSettings
 from ms2deepscore.models.SiameseSpectralModel import SiameseSpectralModel
+from ms2deepscore.__version__ import __version__
 
 
 def load_model(filename: Union[str, Path]) -> SiameseSpectralModel:
@@ -23,7 +24,9 @@ def load_model(filename: Union[str, Path]) -> SiameseSpectralModel:
 
     """
     model_settings = torch.load(filename)
-
+    if model_settings["version"] != __version__:
+        print(f"The model version ({model_settings['version']}) does not match the version of MS2Deepscore "
+              f"({__version__}), consider downloading a new model or changing the MS2Deepscore version")
     # Extract model parameters from the checkpoint
     model_params = model_settings['model_params']
 
