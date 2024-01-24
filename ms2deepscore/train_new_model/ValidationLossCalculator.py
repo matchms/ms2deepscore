9ff709274bcb5ff5c8110194903839919388e3e0
niekdejonge <niekdejonge@gmail.com>
2024-01-24 14:36:40 +0100

    Integrate in train_ms2deepscore.py

--- ms2deepscore/train_new_model/ValidationLossCalculator.py
+++ ms2deepscore/train_new_model/ValidationLossCalculator.py
@@ -9,7 +9,7 @@ from ms2deepscore.models.loss_functions import bin_dependent_losses
 class ValidationLossCalculator:
     def __init__(self,
                  val_spectrums,
-                 score_bins,
+                 score_bins=np.linspace(0, 1.0000001, 11),
                  random_seed=42):
         self.val_spectrums = select_one_spectrum_per_inchikey(val_spectrums, random_seed)
         self.target_scores = get_tanimoto_score_between_spectra(self.val_spectrums, self.val_spectrums)
--- ms2deepscore/train_new_model/train_ms2deepscore.py
+++ ms2deepscore/train_new_model/train_ms2deepscore.py
@@ -12,6 +12,7 @@ from ms2deepscore.SettingsMS2Deepscore import (GeneratorSettings,
                                                SettingsMS2Deepscore, TensorizationSettings)
 from ms2deepscore.train_new_model.spectrum_pair_selection import \
     select_compound_pairs_wrapper
+from ms2deepscore.train_new_model.ValidationLossCalculator import ValidationLossCalculator
 
 
 def train_ms2ds_model(
@@ -47,30 +48,18 @@ def train_ms2ds_model(
         augment_intensity=0.1,
         batch_size=generator_settings.batch_size,
     )
-    selected_compound_pair_val, selected_validation_spectra = select_compound_pairs_wrapper(
-        validation_spectra, settings=generator_settings, shuffling=False)
-    # todo maybe add num_turns=10 # Number of pairs for each InChiKey14 during each epoch. again?
-    val_generator = DataGeneratorPytorch(
-        spectrums=selected_validation_spectra,
-        tensorization_settings=tensoriztion_settings,
-        selected_compound_pairs=selected_compound_pair_val,
-        batch_size=generator_settings.batch_size,
-        use_fixed_set=True,
-        augment_removal_max=0.0,
-        augment_removal_intensity=0.0,
-        augment_intensity=0.0,
-        augment_noise_max=0
-    )
+
+
     # todo check if dropout rate, layers, base dim and embedding size should still be integrated.
     # todo Check where we specify the loss type. Should that happen here as well, maybe settings?
     model = SiameseSpectralModel(tensorisaton_settings=tensoriztion_settings,
                                  train_binning_layer=False)  # TODO: add model_settings 
 
-    history = train(
-        model, train_generator, 200,
-        val_generator=val_generator,
-        checkpoint_filename=output_model_file_name,
-        learning_rate=model_settings.learning_rate, lambda_l1=0, lambda_l2=0, patience=model_settings.patience)
+    validation_loss_calculator = ValidationLossCalculator(validation_spectra)
+    history = train(model, train_generator, 200, learning_rate=model_settings.learning_rate,
+                    validation_loss_calculator=validation_loss_calculator,
+                    patience=model_settings.patience,
+                    checkpoint_filename=output_model_file_name, lambda_l1=0, lambda_l2=0)
     # Save plot of history
     plot_history(history["losses"], history["val_losses"], ms2ds_history_plot_file_name)
 
