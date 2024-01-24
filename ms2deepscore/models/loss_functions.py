1fe7d3c586e1e7fa0b0f9c2d43d47210dc3339f7
niekdejonge <niekdejonge@gmail.com>
2024-01-24 14:24:02 +0100

    Move ValidationLossCalculator to separate file and fix multiple bugs

--- ms2deepscore/models/loss_functions.py
+++ ms2deepscore/models/loss_functions.py
@@ -1,11 +1,6 @@
 import numpy as np
 import torch
-from matchms.similarity.vector_similarity_functions import cosine_similarity_matrix
 from torch import nn
-
-from ms2deepscore.benchmarking.calculate_scores_for_validation import get_tanimoto_score_between_spectra
-from ms2deepscore.models import SiameseSpectralModel
-from ms2deepscore.models.SiameseSpectralModel import compute_embedding_array
 
 
 def rmse_loss(outputs, targets):
@@ -93,10 +88,12 @@ def bin_dependent_losses(predictions,
     loss_types
         Specify list of loss types out of "mse", "mae", "rmse".
     """
+    if predictions.shape != true_values.shape:
+        raise ValueError("Expected true values and predictions to have the same shape")
     bin_content = []
     losses = {"bin": []}
     for loss_type in loss_types:
-        if loss_type not in LOSS_FUNCTIONS:
+        if loss_type.lower() not in LOSS_FUNCTIONS:
             raise ValueError(f"Unknown loss function: {loss_type}. Must be one of: {LOSS_FUNCTIONS.keys()}")
         losses[loss_type] = []
     bounds = []
@@ -106,40 +103,15 @@ def bin_dependent_losses(predictions,
         high = ref_scores_bins_inclusive[i + 1]
         bounds.append((low, high))
         idx = np.where((true_values >= low) & (true_values < high))
+        if idx[0].shape[0] == 0:
+            raise ValueError("No tanimoto scores within bin")
         bin_content.append(idx[0].shape[0])
         # Add values
         losses["bin"].append((low, high))
         for loss_type in loss_types:
-            criterion = LOSS_FUNCTIONS[loss_type]
-            loss = criterion(true_values[idx], predictions[idx])
+            criterion = LOSS_FUNCTIONS[loss_type.lower()]
+            selected_true_values = torch.tensor(true_values[idx])
+            selected_predictions = torch.tensor(predictions[idx])
+            loss = criterion(selected_true_values, selected_predictions)
             losses[loss_type].append(loss)
     return bin_content, bounds, losses
-
-
-class ValidationLossCalculator:
-    def __init__(self,
-                 val_spectrums,
-                 score_bins):
-        # Check if the spectra only are unique inchikeys
-        inchikeys_list = [s.get("inchikey") for s in val_spectrums]
-        assert len(set(inchikeys_list)) == len(val_spectrums), 'Expected 1 spectrum per inchikey'
-        self.target_scores = get_tanimoto_score_between_spectra(val_spectrums, val_spectrums)
-        self.val_spectrums = val_spectrums
-        self.score_bins = score_bins
-
-    def compute_binned_validation_loss(self,
-                                       model: SiameseSpectralModel,
-                                       loss_types):
-        """Benchmark the model against a validation set.
-        """
-        embeddings = compute_embedding_array(model, self.val_spectrums)
-        ms2ds_scores = cosine_similarity_matrix(embeddings, embeddings)
-        losses = bin_dependent_losses(ms2ds_scores,
-                                      self.target_scores,
-                                      self.score_bins,
-                                      loss_types=loss_types
-                                      )
-        average_losses = {}
-        for loss_type in losses:
-            average_losses[loss_type] = np.mean(losses[loss_type])
-        return average_losses
