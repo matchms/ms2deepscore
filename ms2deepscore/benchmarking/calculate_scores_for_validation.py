dec8fb4bfafda34d27e663005e289a32c617b723
Florian Huber <florian.huber@hs-duesseldorf.de>
2024-01-31 13:23:08 +0100

    fix

--- ms2deepscore/benchmarking/calculate_scores_for_validation.py
+++ ms2deepscore/benchmarking/calculate_scores_for_validation.py
@@ -95,7 +95,7 @@ def get_tanimoto_score_between_spectra(spectra_1: List[Spectrum],
     """
     tanimoto_df = calculate_tanimoto_scores_unique_inchikey(spectra_1, spectra_2,
                                                             fingerprint_type,
-                                                            nbits)                                                            )
+                                                            nbits)
     inchikeys_1 = [spectrum.get("inchikey")[:14] for spectrum in spectra_1]
     inchikeys_2 = [spectrum.get("inchikey")[:14] for spectrum in spectra_2]
     tanimoto_scores = tanimoto_df.loc[inchikeys_1, inchikeys_2].values
