5e6fdac39f381365b1db7fbd7f30ff446094479f
Florian Huber <florian.huber@hs-duesseldorf.de>
2024-02-02 09:46:34 +0100

    linting

--- ms2deepscore/benchmarking/plot_ridgeline.py
+++ ms2deepscore/benchmarking/plot_ridgeline.py
@@ -3,11 +3,15 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 
-def create_combined_ridgeline_plot(reference_scores, comparison_scores, n_bins=10,
-                                   min_resolution=20, max_resolution=100,
-                         ref_score_name="Tanimoto similarity", compare_score_name="MS2DeepScore"):
+def create_combined_ridgeline_plot(reference_scores,
+                                   comparison_scores,
+                                   n_bins=10,
+                                   min_resolution=20,
+                                   max_resolution=100,
+                                   ref_score_name="Tanimoto similarity",
+                                   compare_score_name="MS2DeepScore"):
     """
-    Plot histograms to compare reference and comparison scores.
+    Plot ridgeline-style histograms to compare reference and comparison scores.
 
     Parameters
     ----------
@@ -17,21 +21,24 @@ def create_combined_ridgeline_plot(reference_scores, comparison_scores, n_bins=1
         Comparison score array.
     n_bins
         Number of bins. The default is 5.
-    hist_resolution
-        Histogram resolution. The default is 100.
+    min_resolution
+        Minimum histogram resolution. The default is 20.
+    max_resolution
+        The mainimum histogram resolution. The default is 100.
     ref_score_name
         Label string. The default is "Tanimoto similarity".
     compare_score_name
         Label string. The default is "MS2DeepScore".
-
     """
-    histograms, used_bins, bin_content, resolutions = calculate_histograms(reference_scores, comparison_scores,
-                                                                           n_bins, min_resolution, max_resolution)
+    # pylint: disable=too-many-arguments
 
-    fig, (ax_main, ax_hist) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [4, 1]}, figsize=(12, 9))
+    histograms, used_bins, _, _ = calculate_histograms(reference_scores, comparison_scores,
+                                                       n_bins, min_resolution, max_resolution)
+
+    _, (ax_main, ax_hist) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [4, 1]}, figsize=(12, 9))
 
     # Plot ridge plot on main axis
-    ridgeline_plot(histograms, used_bins, resolutions,
+    ridgeline_plot(histograms, used_bins,
                     xlabel=compare_score_name, ylabel=ref_score_name, ax=ax_main)
     ax_hist.set_title("Comparison of Tanimoto scores and predictions.")
 
@@ -43,10 +50,14 @@ def create_combined_ridgeline_plot(reference_scores, comparison_scores, n_bins=1
     plt.show()
 
 
-def ridgeline_plot(histograms, y_score_bins,
-                   resolutions, xlabel="MS2DeepScore", ylabel="Tanimoto similarity",
+def ridgeline_plot(histograms,
+                   y_score_bins,
+                   xlabel="MS2DeepScore",
+                   ylabel="Tanimoto similarity",
                    ax=None):
-    """Create a plot of (partly overlapping) distributions based on score comparison, on specified axis."""
+    """Create a plot of (partly overlapping) distributions based on score comparison, on specified axis.
+    """
+    # pylint: disable=too-many-arguments
     if ax is None:
         ax = plt.gca()
 
@@ -57,12 +68,12 @@ def ridgeline_plot(histograms, y_score_bins,
     alpha = 1.0
 
     for i in range(0, len(histograms)):
-        id = len(histograms)-i-1
-        data = histograms[id][0]
+        idx = len(histograms)-i-1
+        data = histograms[idx][0]
         data = data/max(data)
-        ax.fill_between(histograms[id][1][:-1], -shift*i, [(-shift*i + x) for x in data], color=cmap1(i/10), alpha=alpha)
-        ax.plot(histograms[id][1][:-1], [(-shift*i + x) for x in data], linewidth=2, color="white")
-        ax.plot(histograms[id][1][:-1], [(-shift*i + x) for x in data], ".-", color="gray", alpha=0.5)
+        ax.fill_between(histograms[idx][1][:-1], -shift*i, [(-shift*i + x) for x in data], color=cmap1(i/10), alpha=alpha)
+        ax.plot(histograms[idx][1][:-1], [(-shift*i + x) for x in data], linewidth=2, color="white")
+        ax.plot(histograms[idx][1][:-1], [(-shift*i + x) for x in data], ".-", color="gray", alpha=0.5)
 
     #ax.set_xticks([])
     y_score_bins = [[a, b] for (a, b) in y_score_bins]
@@ -98,13 +109,14 @@ def score_histogram(scores, n_bins, ax=None, ylabel="scores"):
     ax.set_ylabel(ylabel)
     ax.set_ylim([0, (1 + 1/n_bins)])
     ax.set_xscale("log")
-    for i in range(len(bin_content)):
+    for i, bin_value in enumerate(bin_content):
         ax.text(0.05, 1/len(bins) * (i + 0.5), #bins[1]* (i + 0.5),
-                f"{bin_content[i]} pairs", transform=ax.transAxes, ha="left", va="center")
+                f"{bin_value} pairs", transform=ax.transAxes, ha="left", va="center")
 
 
 def calculate_histograms(reference_scores, comparison_scores, n_bins=10, min_resolution=20, max_resolution=100):
     """Calcualte a series of histograms, one for every bin."""
+    # pylint: disable=too-many-locals
     def get_hist_bins(resolution):
         hist_bins = np.linspace(0, 1, resolution)
         hist_bins = np.concatenate((hist_bins, np.array([2.0])))
@@ -117,7 +129,7 @@ def calculate_histograms(reference_scores, comparison_scores, n_bins=10, min_res
     ref_scores_bins_inclusive = np.linspace(0, 1, n_bins+1)
     ref_scores_bins_inclusive[0] = -np.inf
     ref_scores_bins_inclusive[-1] = np.inf
-    
+
     for i in range(n_bins):
         used_bins.append((ref_scores_bins_inclusive[i], ref_scores_bins_inclusive[i+1]))
         idx = np.where((reference_scores >= ref_scores_bins_inclusive[i]) & (reference_scores < ref_scores_bins_inclusive[i+1]))
