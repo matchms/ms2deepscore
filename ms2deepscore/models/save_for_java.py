464951cc394a0a50443cbed6b8cbf97fa7876888
niekdejonge <niekdejonge@gmail.com>
2024-06-26 17:07:17 +0200

    Added create test model for java

--- ms2deepscore/models/save_for_java.py
+++ ms2deepscore/models/save_for_java.py
@@ -1,6 +1,13 @@
+import os
+
 import torch
 
+from ms2deepscore import SettingsMS2Deepscore
 from ms2deepscore.models import load_model
+from ms2deepscore.train_new_model.train_ms2deepscore import train_ms2ds_model
+from tests.create_test_spectra import pesticides_test_spectra
+from tests.test_data_generators import create_test_spectra
+import numpy as np
 
 
 def save_model_for_java(input_model_file_name, output_model_file_name):
@@ -28,13 +35,32 @@ def save_model_for_java_output_embeddings(input_model_file_name, output_model_fi
         model.encoder,
         (torch.rand(nr_of_spectra, model.model_settings.number_of_bins()),
          torch.rand(nr_of_spectra, len(model.model_settings.additional_metadata)),
-         ))
+         )
+    )
 
     # Save the TorchScript model
     traced_script_module.save(output_model_file_name)
 
 
-if __name__ == "__main__":
-    # save_model_for_java("../../../data/pytorch/gnps_21_08_23_min_5_at_5_percent/trained_models/both_mode_precursor_mz_ionmode_2000_2000_2000_layers_500_embedding_2024_01_31_11_51_10/ms2deepscore_model.pt",
-    #                     "../../../data/pytorch/gnps_21_08_23_min_5_at_5_percent/trained_models/both_mode_precursor_mz_ionmode_2000_2000_2000_layers_500_embedding_2024_01_31_11_51_10/ms2deepscore_java_model.pt")
-    save_model_for_java_output_embeddings("../../tests/resources/testmodel.pt", "../../tests/resources/java_ms2deepscore_embedding_model.pt")
+def create_test_model_for_java(results_folder):
+    spectra = create_test_spectra(8)
+    settings = SettingsMS2Deepscore(**{
+        "mz_bin_width": 1.0,
+        "epochs": 2,  # to speed up tests --> usually many more
+        "base_dims": (100, 100),  # to speed up tests --> usually larger
+        "embedding_dim": 50,  # to speed up tests --> usually larger
+        "same_prob_bins": np.array([(0, 0.5), (0.5, 1.0)]),
+        "average_pairs_per_bin": 2,
+        "batch_size": 8,
+        "additional_metadata": [("StandardScaler", {"metadata_field": "precursor_mz",
+                                                    "mean": 0.0,
+                                                    "standard_deviation": 1000.0}),
+                                ("CategoricalToBinary", {"metadata_field": "ionmode",
+                                                         "entries_becoming_one": "positive",
+                                                         "entries_becoming_zero": "negative"})],
+    })
+
+    model_file_name = os.path.join(results_folder, "ms2deepscore_model.pt")
+    train_ms2ds_model(spectra, pesticides_test_spectra(), results_folder, settings)
+
+    save_model_for_java_output_embeddings(model_file_name, os.path.join(results_folder, "java_ms2deepscore_model.pt"))
