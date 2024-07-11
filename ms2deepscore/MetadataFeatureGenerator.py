477c3ff0fb624b4bbe6d922f344d446bbdb5e4f7
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2024-07-11 11:42:07 +0200

    Update MetadataFeatureGenerator.py

--- ms2deepscore/MetadataFeatureGenerator.py
+++ ms2deepscore/MetadataFeatureGenerator.py
@@ -8,31 +8,44 @@ from tqdm import tqdm
 
 
 class MetadataVectorizer:
-    """Create a numerical vector of selected metadata field including transformations..
+    """Create a numerical vector of selected metadata fields including transformations.
+
+    This class is used to transform a list of spectra into numerical vectors
+    based on the specified metadata fields. These vectors can be used for 
+    further analysis or machine learning models.
+
+    Attributes
+    ----------
+    additional_metadata : list of MetadataFeatureGenerator
+        List of metadata feature generators used to create the metadata vector.
     """
 
     def __init__(self, 
                  additional_metadata=()):
         """
-
         Parameters
         ----------
-        additional_metadata:
-            List of all metadata used/wanted in a metadata vector. Default is ().
+        additional_metadata : list of MetadataFeatureGenerator, optional
+            List of metadata feature generators used to create the metadata vector. 
+            Each element in the list should be an instance of a class that inherits from 
+            MetadataFeatureGenerator. Default is an empty tuple.
         """
         self.additional_metadata = additional_metadata
 
     def transform(self, spectra: List[SpectrumType],
                   progress_bar=False):
-        """Transforms the input *spectrums* into metadata vectors as needed for
-        MS2DeepScore.
+        """Transforms the input spectra into metadata vectors as needed for MS2DeepScore.
+
+        This method converts a list of spectra into numerical vectors based on the 
+        specified metadata fields. Each spectrum's metadata is processed through 
+        the feature generators specified in `additional_metadata`.
 
         Parameters
         ----------
-        spectra
-            List of spectra.
-        progress_bar
-            Show progress bar if set to True. Default is False.
+        spectra : list of SpectrumType
+            List of spectra to be transformed.
+        progress_bar : bool, optional
+            Show progress bar if set to True. Default is False..
 
         Returns:
             List of metadata vectors.
@@ -48,17 +61,92 @@ class MetadataVectorizer:
 
     @property
     def size(self):
+Here is the code with more extensive docstrings explaining the usage of additional_metadata and other details:
+
+python
+
+import json
+from importlib import import_module
+from typing import List, Tuple, Union
+import torch
+from matchms import Metadata
+from matchms.typing import SpectrumType
+from tqdm import tqdm
+
+
+class MetadataVectorizer:
+    """Create a numerical vector of selected metadata fields including transformations.
+
+    This class is used to transform a list of spectra into numerical vectors
+    based on the specified metadata fields. These vectors can be used for 
+    further analysis or machine learning models.
+
+    Attributes
+    ----------
+    additional_metadata : list of MetadataFeatureGenerator
+        List of metadata feature generators used to create the metadata vector.
+    """
+
+    def __init__(self, additional_metadata=()):
+        """
+        Parameters
+        ----------
+        additional_metadata : list of MetadataFeatureGenerator, optional
+            List of metadata feature generators used to create the metadata vector. 
+            Each element in the list should be an instance of a class that inherits from 
+            MetadataFeatureGenerator. Default is an empty tuple.
+        """
+        self.additional_metadata = additional_metadata
+
+    def transform(self, spectra: List[SpectrumType], progress_bar=False):
+        """Transforms the input spectra into metadata vectors as needed for MS2DeepScore.
+
+        This method converts a list of spectra into numerical vectors based on the 
+        specified metadata fields. Each spectrum's metadata is processed through 
+        the feature generators specified in `additional_metadata`.
+
+        Parameters
+        ----------
+        spectra : list of SpectrumType
+            List of spectra to be transformed.
+        progress_bar : bool, optional
+            Show progress bar if set to True. Default is False.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor containing the metadata vectors for the input spectra.
+        """
+        metadata_vectors = torch.zeros((len(spectra), self.size))
+        for i, spec in tqdm(enumerate(spectra), desc="Create metadata vectors", disable=(not progress_bar)):
+            metadata_vectors[i, :] = torch.tensor(
+                [feature_generator.generate_features(spec.metadata) for feature_generator in self.additional_metadata]
+            )
+        return metadata_vectors
+
+    @property
+    def size(self):
+        """Returns the size of the metadata vector.
+
+        The size is determined by the number of feature generators specified 
+        in `additional_metadata`.
+        """
         return len(self.additional_metadata)
 
 
 class MetadataFeatureGenerator:
     """Base class to define metadata-to-feature conversion rules.
+
+    This class should be inherited by specific feature generator implementations
+    that define how to convert metadata fields into numerical features.
     """
     def generate_features(self, metadata: Metadata) -> float:
         """This method should be implemented by child classes to generate a input feature for the model"""
         raise NotImplementedError
 
     def to_json(self) -> str:
+        """Convert the feature generator to a JSON string.
+        """
         return json.dumps((type(self).__name__, vars(self)))
 
     @classmethod
@@ -68,13 +156,32 @@ class MetadataFeatureGenerator:
         raise NotImplementedError
 
     def __eq__(self, other):
+        """Check equality with another feature generator.
+
+        Parameters
+        ----------
+        other : MetadataFeatureGenerator
+            The other feature generator to compare with.
+        """
         if isinstance(other, type(self)):
             return self.__dict__ == other.__dict__
         return False
 
 
 class StandardScaler(MetadataFeatureGenerator):
+    """StandardScaler scales metadata by subtracting the mean and dividing by the standard deviation.
+    """
     def __init__(self, metadata_field: str, mean: float, std: float = None):
+        """
+        Parameters
+        ----------
+        metadata_field : str
+            The metadata field to be scaled.
+        mean : float
+            The mean value used for scaling.
+        standard_deviation : float, optional
+            The standard deviation used for scaling. If not provided, only the mean will be subtracted.
+        """
         self.metadata_field = metadata_field
         self.mean = mean
         self.standard_deviation = std
@@ -99,8 +206,18 @@ class StandardScaler(MetadataFeatureGenerator):
 
 
 class OneHotEncoder(MetadataFeatureGenerator):
+    """OneHotEncoder converts categorical metadata to binary (0 or 1) based on specified entries.
+    """
     def __init__(self, metadata_field: str,
                  entries_becoming_one):
+        """
+        Parameters
+        ----------
+        metadata_field : str
+            The metadata field to be encoded.
+        entries_becoming_one : list or str or int
+            The entries in the metadata field that should be encoded as 1.
+        """
         self.metadata_field = metadata_field
         self.entries_becoming_one = entries_becoming_one
 
