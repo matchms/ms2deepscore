d70fe993ca5b9b6619455b0311148c836a89e17b
niekdejonge <niekdejonge@gmail.com>
2024-09-17 20:54:28 +0200

    Restructure tests in test_inchikey_pair_selection.py and add tests for oversampling

--- tests/test_inchikey_pair_selection.py
+++ tests/test_inchikey_pair_selection.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import numpy as np
 import pytest
 from matchms import Spectrum
@@ -7,7 +9,8 @@ from ms2deepscore import SettingsMS2Deepscore
 from ms2deepscore.train_new_model.inchikey_pair_selection import (
     compute_jaccard_similarity_per_bin, convert_pair_array_to_coo_array,
     SelectedInchikeyPairs,
-    select_inchi_for_unique_inchikeys, select_compound_pairs_wrapper)
+    select_inchi_for_unique_inchikeys, select_compound_pairs_wrapper, compute_fingerprints_for_training,
+    convert_selected_pairs_matrix)
 from tests.create_test_spectra import create_test_spectra
 
 
@@ -36,7 +39,7 @@ def fingerprints():
 
 
 @pytest.fixture
-def spectrums():
+def test_spectra():
     metadata = {"precursor_mz": 101.1,
                 "inchikey": "ABCABCABCABCAB-nonsense",
                 "inchi": "InChI=1/C6H8O6/c7-1-2(8)5-3(9)4(10)6(11)12-5/h2,5,7-10H,1H2/t2-,5+/m0/s1"}
@@ -119,17 +122,17 @@ def test_compute_jaccard_similarity_per_bin_correct_counts(fingerprints):
         assert np.all(matrix_histogram[0] == expected_histogram)
 
 
-def test_select_inchi_for_unique_inchikeys(spectrums):
-    spectrums[2].set("inchikey", "ABCABCABCABCAB-nonsense2")
-    spectrums[3].set("inchikey", "ABCABCABCABCAB-nonsense3")
-    (spectrums_selected, inchikey14s) = select_inchi_for_unique_inchikeys(spectrums)
+def test_select_inchi_for_unique_inchikeys(test_spectra):
+    test_spectra[2].set("inchikey", "ABCABCABCABCAB-nonsense2")
+    test_spectra[3].set("inchikey", "ABCABCABCABCAB-nonsense3")
+    (spectrums_selected, inchikey14s) = select_inchi_for_unique_inchikeys(test_spectra)
     assert inchikey14s == ['ABCABCABCABCAB']
     assert spectrums_selected[0].get("inchi").startswith("InChI=1/C6H8O6/")
 
 
-def test_select_inchi_for_unique_inchikeys_two_inchikeys(spectrums):
+def test_select_inchi_for_unique_inchikeys_two_inchikeys(test_spectra):
     # Test for two different inchikeys
-    (spectrums_selected, inchikey14s) = select_inchi_for_unique_inchikeys(spectrums)
+    (spectrums_selected, inchikey14s) = select_inchi_for_unique_inchikeys(test_spectra)
     assert inchikey14s == ['ABCABCABCABCAB', 'XXXXXXXXXXXXXX']
     assert [s.get("inchi")[:15] for s in spectrums_selected] == ['InChI=1/C6H8O6/', 'InChI=1S/C8H10N']
 
@@ -164,46 +167,118 @@ def test_SelectedInchikeyPairs_generator_without_shuffle(dummy_spectrum_pairs):
         assert expected_pair == next(gen)
 
 
-@pytest.fixture
-def dummy_selected_inchikey_pairs() -> SelectedInchikeyPairs:
-    spectrums = create_test_spectra(num_of_unique_inchikeys=17, num_of_spectra_per_inchikey=2)
-    settings = SettingsMS2Deepscore(same_prob_bins=np.array([(-0.000001, 0.25), (0.25, 0.5), (0.5, 0.75), (0.75, 1)]),
-                                    average_pairs_per_bin=2,
-                                    batch_size=8)
-    return select_compound_pairs_wrapper(spectrums, settings)
+def test_select_compound_pairs_wrapper_no_resampling():
+    spectrums = create_test_spectra(num_of_unique_inchikeys=26, num_of_spectra_per_inchikey=2)
+    bins = [(0.5, 0.75), (0.25, 0.5), (0.75, 1), (-0.000001, 0.25)]
+    max_pair_resampling = 1
+    settings = SettingsMS2Deepscore(same_prob_bins=np.array(bins),
+                                    average_pairs_per_bin=5,
+                                    batch_size=8,
+                                    max_pair_resampling=max_pair_resampling)
+    selected_inchikey_pairs = select_compound_pairs_wrapper(spectrums, settings)
+
+    check_balanced_scores_selecting_inchikey_pairs(selected_inchikey_pairs, bins)
+    check_balanced_inchikey_count_selecting_inchikey_pairs(selected_inchikey_pairs)
+    print_balanced_bins_per_inchikey(selected_inchikey_pairs, settings, spectrums)
+    check_correct_oversampling(selected_inchikey_pairs, max_pair_resampling)
 
 
-def test_balanced_inchikey_count_selecting_inchikey_pairs(dummy_selected_inchikey_pairs):
+def test_select_compound_pairs_wrapper_with_resampling():
+    spectrums = create_test_spectra(num_of_unique_inchikeys=26, num_of_spectra_per_inchikey=1)
+    bins = [(0.8, 0.9), (0.7, 0.8), (0.9, 1.0), (0.6, 0.7), (0.5, 0.6),
+            (0.4, 0.5), (0.3, 0.4), (0.2, 0.3), (0.1, 0.2), (-0.01, 0.1)]
+    max_pair_resampling = 10
+    settings = SettingsMS2Deepscore(same_prob_bins=np.array(bins, dtype="float32"),
+                                    average_pairs_per_bin=10,
+                                    batch_size=8,
+                                    max_pair_resampling=max_pair_resampling)
+    selected_inchikey_pairs = select_compound_pairs_wrapper(spectrums, settings)
+
+    check_balanced_scores_selecting_inchikey_pairs(selected_inchikey_pairs, bins)
+    check_balanced_inchikey_count_selecting_inchikey_pairs(selected_inchikey_pairs)
+    # Currently doesn't check anything, but prints badly distributed pairs and the available pairs. It is hard to write
+    # a good test, since the balancing behaviour we would like to see only happens when you have a lot more pairs
+    # (and inchikeys) which is not suitable for a test.
+    print_balanced_bins_per_inchikey(selected_inchikey_pairs, settings, spectrums)
+    check_correct_oversampling(selected_inchikey_pairs, max_pair_resampling)
+
+
+def check_correct_oversampling(selected_inchikey_pairs: SelectedInchikeyPairs, max_resampling: int):
+    pair_counts = Counter(selected_inchikey_pairs.selected_inchikey_pairs)
+    for count in pair_counts.values():
+        assert count <= max_resampling, "the resampling was done too frequently"
+
+
+def get_available_score_distribution(settings, spectra):
+    """Gets the score distribution for the available pairs (before doing balanced selection)"""
+    fingerprints, inchikeys14_unique = compute_fingerprints_for_training(
+        spectra,
+        settings.fingerprint_type,
+        settings.fingerprint_nbits)
+
+    available_pairs_per_bin_matrix, available_scores_per_bin_matrix = compute_jaccard_similarity_per_bin(
+        fingerprints,
+        settings.max_pairs_per_bin,
+        settings.same_prob_bins,
+        settings.include_diagonal)
+
+    available_pairs_per_bin = convert_selected_pairs_matrix(available_pairs_per_bin_matrix,
+                                                            available_scores_per_bin_matrix, inchikeys14_unique)
+
+    score_distribution_per_inchikey = {inchikey: [0]*len(settings.same_prob_bins) for inchikey in inchikeys14_unique}
+    for available_pairs in available_pairs_per_bin:
+        for inchikey_1, inchikey_2, score in available_pairs:
+            for i, score_bin in enumerate(settings.same_prob_bins):
+                if score > score_bin[0] and score <= score_bin[1]:
+                    score_distribution_per_inchikey[inchikey_1][i] += 1
+                    score_distribution_per_inchikey[inchikey_2][i] += 1
+    return score_distribution_per_inchikey
+
+
+def print_balanced_bins_per_inchikey(selected_inchikey_pairs: SelectedInchikeyPairs, settings, spectra):
+    """Prints the availabl distribution and the balanced distribution
+
+    Currently doesn't do any checks, because it is hard to check if the wanted behaviour is achieved,
+    since it is different for small test sets compared to large test sets."""
+    score_distribution_per_inchikey = {inchikey: [0]*len(settings.same_prob_bins) for inchikey in selected_inchikey_pairs.get_inchikey_counts().keys()}
+    for inchikey_1, inchikey_2, score in selected_inchikey_pairs.selected_inchikey_pairs:
+        for i, score_bin in enumerate(settings.same_prob_bins):
+            if score > score_bin[0] and score <= score_bin[1]:
+                score_distribution_per_inchikey[inchikey_1][i] += 1
+                score_distribution_per_inchikey[inchikey_2][i] += 1
+    available_score_distribution = get_available_score_distribution(settings, spectra)
+
+    for inchikey in score_distribution_per_inchikey.keys():
+        balanced_distribution = score_distribution_per_inchikey[inchikey]
+        average_balanced_distribution = sum(balanced_distribution)/len(balanced_distribution)
+        if min(balanced_distribution)*2 < average_balanced_distribution:
+            available_distribution = available_score_distribution[inchikey]
+            index_of_min = balanced_distribution.index(min(balanced_distribution))
+            print(available_distribution, balanced_distribution)
+            # assert minimum_available_distribution*settings.max_pair_resampling == min(balanced_distribution)
+
+
+def check_balanced_inchikey_count_selecting_inchikey_pairs(selected_inchikey_pairs: SelectedInchikeyPairs):
     """Test if SelectedInchikeyPairs has an equal inchikey distribution
     """
-    inchikey_counts = dummy_selected_inchikey_pairs.get_inchikey_counts()
+    inchikey_counts = selected_inchikey_pairs.get_inchikey_counts()
     max_difference_in_inchikey_freq = max(inchikey_counts.values()) - min(inchikey_counts.values())
-    assert max_difference_in_inchikey_freq <= 2, "The frequency of the sampling of the inchikeys is too different"
+    assert max_difference_in_inchikey_freq < max(inchikey_counts.values())/2, "The frequency of the sampling of the inchikeys is too different"
 
 
-def test_balanced_scores_selecting_inchikey_pairs(dummy_selected_inchikey_pairs):
+def check_balanced_scores_selecting_inchikey_pairs(selected_inchikey_pairs: SelectedInchikeyPairs,
+                                                   score_bins):
     """Test if SelectedInchikeyPairs has an equal inchikey distribution
     """
-    scores = dummy_selected_inchikey_pairs.get_scores()
-    score_bins = [(-0.000001, 0.25), (0.25, 0.5), (0.5, 0.75), (0.75, 1)]
-    score_bin_counts = {score_bin: 0 for score_bin in score_bins}
+    scores = selected_inchikey_pairs.get_scores()
+    # converting to float32 is required, since the scores are float32, otherwise equal numbers are seen as not equal
+    # and put in the wrong bin.
+    score_bins = np.array(score_bins, dtype="float32")
+    score_bin_counts = {tuple(score_bin): 0 for score_bin in score_bins}
     for score in scores:
-        for min_bound, max_bound in score_bin_counts.keys():
+        for min_bound, max_bound in score_bins:
             if score > min_bound and score <= max_bound:
                 score_bin_counts[(min_bound, max_bound)] += 1
     # Check that the number of pairs per bin is equal for all bins
     assert len(set(score_bin_counts.values())) == 1
 
-
-def test_no_repeating_of_pairs_when_selecting_inchikey_pairs(dummy_selected_inchikey_pairs):
-    """Pairs are stored as inchikey1, inchikey2 score, this test checks that no pairs reappear,
-    including the reverse pairs like inchikey2, inchikey1"""
-    pairs = dummy_selected_inchikey_pairs.selected_inchikey_pairs
-    for inchikey1_check, inchikey2_check, _ in pairs:
-        count_of_pair = 0
-        for inchikey1, inchikey2, _ in pairs:
-            if inchikey1 == inchikey1_check and inchikey2 == inchikey2_check:
-                count_of_pair += 1
-            elif inchikey1 == inchikey2_check and inchikey2 == inchikey1_check:
-                count_of_pair += 1
-        assert count_of_pair == 1, "The pair occurs multiple times in the selected pairs (likely in reversed form)"
