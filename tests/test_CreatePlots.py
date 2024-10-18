a33dc9f6c1fa5b73f64105b6a492721be2dae58f
niekdejonge <niekdejonge@gmail.com>
2024-10-18 15:24:12 +0200

    Add create_dummy_predictions_and_tanimoto_scores

--- tests/test_CreatePlots.py
+++ tests/test_CreatePlots.py
@@ -1,3 +1,7 @@
+import string
+
+import numpy as np
+import pandas as pd
 import pytest
 
 from ms2deepscore.benchmarking.CalculateScoresBetweenAllIonmodes import CalculateScoresBetweenAllIonmodes, PredictionsAndTanimotoScores
@@ -7,13 +11,55 @@ import random
 from ms2deepscore.benchmarking.plot_average_per_bin import plot_average_per_bin
 
 
-def testCreatePlots():
+def dummy_tanimoto_scores(num_of_unique_inchikeys):
+    # Create list of dummy_inchikeys, with the same letter repeating
+    dummy_inchikeys = [f"{14 * letter}" for letter in list(string.ascii_uppercase[:num_of_unique_inchikeys])]
+    # Generate random values
+    random_values = np.random.rand(len(dummy_inchikeys), len(dummy_inchikeys))
+    # Make the matrix symmetric
+    symmetric_values = (random_values + random_values.T) / 2
+    # Create the DataFrame with tanimoto_scores
+    tanimoto_scores = pd.DataFrame(symmetric_values, index=dummy_inchikeys, columns=dummy_inchikeys)
+    # Make sure all the diagonals are 1
+    np.fill_diagonal(tanimoto_scores.values, 1)
+    return tanimoto_scores
+
+
+def create_dummy_predictions(tanimoto_scores: pd.DataFrame):
+    predictions = tanimoto_scores.__deepcopy__()
+    inchikeys = list(predictions.index)
+    random_half_of_inchikeys = random.sample(inchikeys, len(inchikeys)//2)
+    for inchikey in random_half_of_inchikeys:
+        new_row = tanimoto_scores.loc[inchikey].copy()
+        new_row.name = inchikey  # Set the name for the new row
+        predictions = pd.concat([predictions, pd.DataFrame([new_row])])
+    for inchikey in random_half_of_inchikeys:
+        predictions["new_copy"] = predictions[inchikey]
+        predictions.rename(columns={'new_copy': inchikey}, inplace=True)
+    random_noise = np.random.normal(loc=0, scale=0.1, size=predictions.shape)
+    predictions = predictions + random_noise
+    symmetric_predictions = (predictions + predictions.T) / 2
+    return symmetric_predictions
+
+
+def create_dummy_predictions_and_tanimoto_scores(nr_of_unique_inchikeys):
+    tanimoto_scores = dummy_tanimoto_scores(nr_of_unique_inchikeys)
+    predictions = create_dummy_predictions(tanimoto_scores)
+    return PredictionsAndTanimotoScores(predictions, tanimoto_scores, True)
+
+
+def test_predictions_and_tanimoto_scores():
+    predictions_and_tanimoto_scores = create_dummy_predictions_and_tanimoto_scores(26)
+    # todo add actual tests for correctly creating other files
+
+
+def test_calculate_scores_between_all_ionmodes():
     spectrums = load_processed_spectrums()
 
     # Load pretrained model
     model_file = TEST_RESOURCES_PATH / "testmodel.pt"
-    plots_creator = CalculateScoresBetweenAllIonmodes(model_file, spectrums, spectrums)
-
+    scores_between_all_ionmodes = CalculateScoresBetweenAllIonmodes(model_file, spectrums, spectrums)
+    print(scores_between_all_ionmodes)
 
 @pytest.fixture()
 def dummy_scores():
