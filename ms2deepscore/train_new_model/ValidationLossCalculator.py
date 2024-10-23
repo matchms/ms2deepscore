e0942e31fbe0bbd816426832c4475f9ab01f2153
niekdejonge <niekdejonge@gmail.com>
2024-10-23 13:40:24 +0200

    Move calculate_scores_for_validation functions to separate file

--- ms2deepscore/train_new_model/ValidationLossCalculator.py
+++ ms2deepscore/train_new_model/ValidationLossCalculator.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 from ms2deepscore import MS2DeepScore
-from ms2deepscore.benchmarking.CalculateScoresBetweenAllIonmodes import calculate_tanimoto_scores_unique_inchikey, \
-    create_embedding_matrix_symmetric
+from ms2deepscore.benchmarking.calculate_scores_for_validation import create_embedding_matrix_symmetric, \
+    calculate_tanimoto_scores_unique_inchikey
 from ms2deepscore.benchmarking.PredictionsAndTanimotoScores import PredictionsAndTanimotoScores
 from ms2deepscore.models.SiameseSpectralModel import (SiameseSpectralModel,
                                                       compute_embedding_array)
