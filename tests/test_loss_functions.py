fa8af7fd5c4d56b4fd091a905a3a3d60f3c76b99
niekdejonge <niekdejonge@gmail.com>
2024-10-24 13:22:04 +0200

    Remove bin_dependent_losses (since not used anymore)

--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -2,8 +2,7 @@ import numpy as np
 import torch
 from ms2deepscore.models.loss_functions import (risk_aware_mae, risk_aware_mse,
                                                 rmse_loss, RiskAwareMAE,
-                                                mae_loss, mse_loss,
-                                                bin_dependent_losses)
+                                                mae_loss, mse_loss)
 
 
 def test_mae_loss():
@@ -64,20 +63,3 @@ def test_risk_aware_mae_class():
     targets = torch.tensor([0.3, 0.5, 0.7])
     assert model(outputs, targets) == risk_aware_mae(outputs, targets), "Class-based Risk-aware MAE mismatch"
 
-
-def test_bin_dependent_losses():
-    predictions = np.array([0.2, 0.4, 0.6, 0.8])
-    true_values = np.array([0.3, 0.5, 0.7, 0.9])
-    ref_score_bins = [(0.0, 0.5), (0.5, 1.0)]
-    loss_types = ["mse", "mae"]
-    bin_content, _, losses = bin_dependent_losses(predictions, true_values, ref_score_bins, loss_types)
-    assert len(bin_content) == 2, "Bin content length mismatch"
-    assert len(losses["mse"]) == 2, "Losses for MSE mismatch"
-    assert len(losses["mae"]) == 2, "Losses for MAE mismatch"
-    assert bin_content == [2, 2]
-    assert losses["bin"] == ref_score_bins
-    # Check if the losses are as expected.
-    assert torch.allclose(losses["mse"][0], torch.tensor(0.01, dtype=torch.float64))
-    assert torch.allclose(losses["mse"][1], torch.tensor(0.01, dtype=torch.float64))
-    assert torch.allclose(losses["mae"][0], torch.tensor(0.1, dtype=torch.float64))
-    assert torch.allclose(losses["mae"][1], torch.tensor(0.1, dtype=torch.float64))
