b4a107dda7d2ae65ef8610d5cefd3fcfb4477ad3
niekdejonge <niekdejonge@gmail.com>
2024-10-24 09:49:49 +0200

    Remove the need for convert dataframes to lists with matching pairs in plot average per bin

--- ms2deepscore/benchmarking/plot_average_per_bin.py
+++ ms2deepscore/benchmarking/plot_average_per_bin.py
@@ -4,28 +4,22 @@ from tqdm import tqdm
 from matplotlib import pyplot as plt
 
 from ms2deepscore.benchmarking.CalculateScoresBetweenAllIonmodes import CalculateScoresBetweenAllIonmodes
-from ms2deepscore.validation_loss_calculation.PredictionsAndTanimotoScores import PredictionsAndTanimotoScores, \
-    convert_dataframes_to_lists_with_matching_pairs
+from ms2deepscore.validation_loss_calculation.PredictionsAndTanimotoScores import PredictionsAndTanimotoScores
 
 
 def select_pairs_per_bin(predictions_and_tanimoto_scores: PredictionsAndTanimotoScores, bins):
-    average_predictions = predictions_and_tanimoto_scores.get_average_prediction_per_inchikey_pair()
-    # Convert to two lists containing the predictions and tanimoto scores they are sorted
-    list_of_average_predictions, list_of_tanimoto_scores = convert_dataframes_to_lists_with_matching_pairs(
-        predictions_and_tanimoto_scores.tanimoto_df, average_predictions)
+    average_predictions = predictions_and_tanimoto_scores.get_average_prediction_per_inchikey_pair().to_numpy()
 
-    digitized = np.digitize(list_of_tanimoto_scores, bins) - 1
+    digitized = np.digitize(predictions_and_tanimoto_scores.tanimoto_df, bins) - 1
     average_per_bin = []
     for bin in tqdm(range(len(bins)-1), desc="Selecting available inchikey pairs per bin"):
-        predictions_in_this_bin = []
-        indexes_of_pairs_in_bin = np.where(digitized == bin)[0]
-        for i in indexes_of_pairs_in_bin:
-            predictions_in_this_bin.append(list_of_average_predictions[i])
+        row_idxs, col_idxs = np.where(digitized == bin)
+        predictions_in_this_bin = average_predictions[row_idxs, col_idxs]
         if len(predictions_in_this_bin) == 0:
             average_per_bin.append(0)
             print(f"The bin between {bins[bin]} - {bins[bin + 1]}does not have any pairs")
         else:
-            average_per_bin.append(sum(predictions_in_this_bin)/len(predictions_in_this_bin))
+            average_per_bin.append(predictions_in_this_bin.mean())
     return average_per_bin
 
 
