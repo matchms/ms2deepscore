d5ef9fb24f14fee61a2e1f9ce9765a360c2e7f14
niekdejonge <niekdejonge@gmail.com>
2024-10-25 08:49:02 +0200

    Remove unused function

--- ms2deepscore/wrapper_functions/plotting_wrapper_functions.py
+++ ms2deepscore/wrapper_functions/plotting_wrapper_functions.py
@@ -1,7 +1,5 @@
 import os
-import sys
 from matplotlib import pyplot as plt
-from tqdm import tqdm
 
 from ms2deepscore.benchmarking.CalculateScoresBetweenAllIonmodes import CalculateScoresBetweenAllIonmodes
 from ms2deepscore.benchmarking.plot_average_per_bin import plot_average_per_bin
@@ -55,41 +53,3 @@ def create_plots_between_ionmodes(positive_validation_spectra,
 
     fig = plot_average_per_bin(scores_between_all_ionmodes, nr_of_bins)
     fig.savefig(os.path.join(results_folder, "average_per_bin.svg"))
-
-
-def create_plots_for_all_models(models_directory,
-                                results_folder=None,
-                                nr_of_bins=10):
-    """Creates the plots for all models in a directory
-
-    models_directory:
-        A folder with directories containing trained ms2deepscore models
-    results_folder:
-        The folder to store the plots, if None, it will be stored in the folders of the models under benchmarking/plots"""
-
-    for data_processing_dir in os.listdir(models_directory):
-        trained_models_dir = os.path.join(models_directory, data_processing_dir, "trained_models")
-        if os.path.isdir(trained_models_dir):
-            for model_dir_name in tqdm(os.listdir(trained_models_dir),
-                                       desc=f"Creation plots for all models in {trained_models_dir}"):
-                if results_folder is not None:
-                    results_folder_per_model = os.path.join(results_folder, data_processing_dir, model_dir_name)
-                else:
-                    results_folder_per_model = None
-                create_plots_between_ionmodes(os.path.join(trained_models_dir, model_dir_name),
-                                              results_folder_per_model, nr_of_bins=nr_of_bins)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) not in (2, 3):
-        print("Could not run, please provide the model folder")
-        sys.exit(1)
-
-    model_directory = sys.argv[1]
-    if len(sys.argv) == 3:
-        results_folder = sys.argv[2]
-    else:
-        results_folder = None
-
-    create_plots_for_all_models(models_directory=model_directory,
-                                results_folder=results_folder)
