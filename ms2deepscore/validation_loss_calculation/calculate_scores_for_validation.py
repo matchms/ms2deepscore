e9b4bb8396c03fbeef0a365db7b3284045f7417b
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2024-10-28 17:48:15 +0100

    linting & docstrings

--- ms2deepscore/validation_loss_calculation/calculate_scores_for_validation.py
+++ ms2deepscore/validation_loss_calculation/calculate_scores_for_validation.py
@@ -11,52 +11,92 @@ from ms2deepscore.train_new_model.inchikey_pair_selection import select_inchi_fo
 from ms2deepscore.vector_operations import cosine_similarity_matrix
 
 
-def create_embedding_matrix_symmetric(model, spectra):
+def create_embedding_matrix_symmetric(model, spectra): -> pd.DataFrame
+    """
+    Create a symmetric embedding matrix by calculating the cosine similarity between embeddings.
+
+    Parameters:
+    -----------
+    model : MS2DeepScore model instance
+        The model used to generate embeddings.
+    spectra : list of Spectrum
+        A list of spectra for which embeddings will be generated.
+    """
     print("Calculating embeddings")
     embeddings = model.get_embedding_array(spectra)
+
     print("Calculating similarity between embeddings")
     predictions = cosine_similarity_matrix(embeddings, embeddings)
-    # Select the inchikeys per spectrum
+
+    # Extract InChIKeys (first 14 characters) for each spectrum
     inchikeys = []
     for spectrum in spectra:
         inchikeys.append(spectrum.get("inchikey")[:14])
-    # create dataframe with inchikeys as indexes
+
+    # Create DataFrame with InChIKeys as indexes and columns
     predictions_df = pd.DataFrame(predictions, index=inchikeys, columns=inchikeys)
     return predictions_df
 
 
-def create_embedding_matrix_not_symmetric(model, spectra, spectra_2):
+def create_embedding_matrix_not_symmetric(model, spectra_1, spectra_2): -> pd.DataFrame
+    """
+    Create a non-symmetric embedding matrix by calculating the cosine similarity between embeddings of two different sets.
+
+    Parameters:
+    -----------
+    model : MS2DeepScore model instance
+        The model used to generate embeddings.
+    spectra_1:
+        A list of spectra for which embeddings will be generated.
+    spectra_2:
+        A second list of spectra for which embeddings will be generated.
+    """
     print("Calculating embeddings")
     embeddings1 = model.get_embedding_array(spectra)
     embeddings2 = model.get_embedding_array(spectra_2)
-    print("Calculating similarity between embeddings")
 
+    print("Calculating similarity between embeddings")
     predictions = cosine_similarity_matrix(embeddings1, embeddings2)
-    # Select the inchikeys per spectrum
-    inchikeys1 = [spectrum.get("inchikey")[:14] for spectrum in spectra]
+
+    # Extract InChIKeys (first 14 characters) for each spectrum
+    inchikeys1 = [spectrum.get("inchikey")[:14] for spectrum in spectra_1]
     inchikeys2 = [spectrum.get("inchikey")[:14] for spectrum in spectra_2]
 
-    # create dataframe with inchikeys as indexes
+    # Create DataFrame with InChIKeys as indexes and columns
     predictions_df = pd.DataFrame(predictions, index=inchikeys1, columns=inchikeys2)
     return predictions_df
 
 
-def calculate_tanimoto_scores_unique_inchikey(list_of_spectra_1: List[Spectrum],
-                                              list_of_spectra_2: List[Spectrum],
-                                              fingerprint_type="daylight",
-                                              nbits=2048
-                                              ):
-    """Returns a dataframe with the tanimoto scores between each unique inchikey in list of spectra"""
+def calculate_tanimoto_scores_unique_inchikey(
+    list_of_spectra_1: List[Spectrum],
+    list_of_spectra_2: List[Spectrum],
+    fingerprint_type="daylight",
+    nbits=2048
+    ): -> pd.DataFrame
+    """
+    Calculate the Tanimoto scores between unique InChIKeys in two lists of spectra.
 
+    Parameters:
+    -----------
+    list_of_spectra_1 : List[Spectrum]
+        A list of spectra for the first set.
+    list_of_spectra_2 : List[Spectrum]
+        A list of spectra for the second set.
+    fingerprint_type : str, optional
+        The type of fingerprint to derive (default is "daylight").
+    nbits : int, optional
+        The number of bits for the fingerprint (default is 2048).
+    """
     def get_fingerprint(smiles: str):
         fingerprint = _derive_fingerprint_from_smiles(smiles,
                                                       fingerprint_type=fingerprint_type,
                                                       nbits=nbits)
-        assert isinstance(fingerprint, np.ndarray), f"Fingerprint could not be set smiles is {smiles}"
+        if not isinstance(fingerprint, np.ndarray):
+            raise ValueError(f"Fingerprint could not be set for SMILES: {smiles}")
         return fingerprint
 
-    if len(list_of_spectra_1) == 0 or len(list_of_spectra_2) == 0:
-        raise ValueError("The nr of spectra to calculate tanimoto scores should be larger than 0")
+    if (len(list_of_spectra_1) == 0) or (len(list_of_spectra_2) == 0):
+        raise ValueError("The number of spectra to calculate Tanimoto scores should be larger than 0")
 
     spectra_with_most_frequent_inchi_per_inchikey_1, unique_inchikeys_1 = \
         select_inchi_for_unique_inchikeys(list_of_spectra_1)
