90579e57a7f9618aedcaa4b379377b98cd073ad5
Florian Huber <florian.huber@hs-duesseldorf.de>
2026-03-26 14:32:03 +0100

    adjust metric to different fingerprint types

--- ms2deepscore/validation_loss_calculation/calculate_scores_for_validation.py
+++ ms2deepscore/validation_loss_calculation/calculate_scores_for_validation.py
@@ -4,7 +4,12 @@ import numpy as np
 import pandas as pd
 from matchms import Spectrum
 from tqdm import tqdm
-from chemap.metrics import tanimoto_similarity_matrix_dense
+from chemap.metrics import (
+    tanimoto_similarity_matrix_dense,
+    tanimoto_similarity_matrix_sparse_binary,
+    tanimoto_similarity_matrix_sparse
+)
+
 
 from ms2deepscore.train_new_model.inchikey_pair_selection import select_inchi_for_unique_inchikeys
 from ms2deepscore.vector_operations import cosine_similarity_matrix
@@ -87,16 +92,6 @@ def calculate_tanimoto_scores_unique_inchikey(
     nbits : int, optional
         The number of bits for the fingerprint (default is 2048).
     """
-    def get_fingerprint(smiles: str):
-        fingerprint = derive_fingerprint_from_smiles(
-            smiles,
-            fingerprint_type=fingerprint_type,
-            nbits=nbits
-        )
-        if not isinstance(fingerprint, np.ndarray):
-            raise ValueError(f"Fingerprint could not be set for SMILES: {smiles}")
-        return fingerprint
-
     if (len(list_of_spectra_1) == 0) or (len(list_of_spectra_2) == 0):
         raise ValueError("The number of spectra to calculate Tanimoto scores should be larger than 0")
 
@@ -108,15 +103,30 @@ def calculate_tanimoto_scores_unique_inchikey(
     list_of_smiles_1 = [matchms_spectrum_to_smiles(spectrum) for spectrum in spectra_with_most_frequent_inchi_per_inchikey_1]
     list_of_smiles_2 = [matchms_spectrum_to_smiles(spectrum) for spectrum in spectra_with_most_frequent_inchi_per_inchikey_2]
 
-    fingerprints_1 = np.array([get_fingerprint(smiles) for smiles in tqdm(
-        list_of_smiles_1,
-        desc="Calculating fingerprints")]
+    fingerprints_1 = derive_fingerprint_from_smiles(
+            list_of_smiles_1,
+            fingerprint_type=fingerprint_type,
+            nbits=nbits
         )
-    fingerprints_2 = np.array([get_fingerprint(smiles) for smiles in tqdm(
-        list_of_smiles_2,
-        desc="Calculating fingerprints")]
+    fingerprints_2 = derive_fingerprint_from_smiles(
+            list_of_smiles_2,
+            fingerprint_type=fingerprint_type,
+            nbits=nbits
         )
     print("Calculating tanimoto scores")
-    tanimoto_scores = tanimoto_similarity_matrix_dense(fingerprints_1, fingerprints_2)
+
+    if "unfolded" in fingerprint_type:
+        if "count" in fingerprint_type:
+            tanimoto_scores = tanimoto_similarity_matrix_sparse(
+                [x[0] for x in fingerprints_1],
+                [x[1] for x in fingerprints_1],
+                [x[0] for x in fingerprints_2],
+                [x[1] for x in fingerprints_2],
+                )
+        else:
+            tanimoto_scores = tanimoto_similarity_matrix_sparse_binary(fingerprints_1, fingerprints_2)
+    
+    else:
+        tanimoto_scores = tanimoto_similarity_matrix_dense(fingerprints_1, fingerprints_2)
     tanimoto_df = pd.DataFrame(tanimoto_scores, index=unique_inchikeys_1, columns=unique_inchikeys_2)
     return tanimoto_df
